84b0d109c4465d526f7047e0d3f9ee6959286317
liyuepeng <122339792@qq.com>
2018-10-22 17:46:33 +0800

    删除旧的web构建项目

--- erupt-web-build/src/app/erupt/model/erupt.const.ts
+++
@@ -1,4 +0,0 @@
-/**
- * Created by liyuepeng on 10/19/18.
- */
-export const EDIT_VAL_KEY = "$value";
--- erupt-web-build/src/app/theme/widget/statistic/statistic.component.ts
+++
@@ -1,1 +0,0 @@
-import {Component, OnInit} from '@angular/core';@Component({  selector: 'app-statistic',  templateUrl: './statistic.component.html',  styleUrls: ['./statistic.component.scss']})export class StatisticComponent implements OnInit {  constructor() { }  ngOnInit() {  }}
