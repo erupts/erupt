6392cf4bd6141608cb89f80e96065e327d03d681
122339792@qq.com <122339792@qq.com>
2019-05-07 11:11:18 +0800

    添加注释

--- erupt-annotation/src/main/java/xyz/erupt/annotation/sub_field/sub_edit/TabEnum.java
+++ erupt-annotation/src/main/java/xyz/erupt/annotation/sub_field/sub_edit/TabEnum.java
@@ -4,7 +4,9 @@ package xyz.erupt.annotation.sub_field.sub_edit;
  * Created by liyuepeng on 2019-01-10.
  */
 public enum TabEnum {
+    //多对多的情况下使用Tree,一对多的情况下使用Table
     TABLE,
-    LIST_SELECT,
     TREE
+//    LIST_SELECT,
+
 }
