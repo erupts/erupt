b7f4719c53ac0948b1fa058d5d7de317bb81572a
122339792@qq.com <122339792@qq.com>
2019-09-15 19:03:40 +0800

    更新web模块

--- erupt-web/src/main/resources/public/assets/app.js
+++ erupt-web/src/main/resources/public/assets/app.js
@@ -1,20 +1,21 @@
 window.eruptSiteConfig = {
     //global config
-    domain: "http://localhost:8888",
-    title: "YuePeng Erupt",
+    domain: "http://localhost:9999",
+    title: "YuePeng Erupt", //通用数据管理框架
     desc: "通用数据管理框架",
     routerReuse: false,
     amapKey: "15bc4df0bd6758645308fa591f439799",
     logoPath: "/assets/logo.svg",
     r_tools: [{
-            text: "下载",
-            icon: "fa-download",
-            mobileHidden: true,
-            click: function (event, token) {
-                console.log(event);
-                console.log(token);
-            },
-            load: function (event, token) {
-            }
-        }]
+        text: "下载",
+        icon: "fa-download",
+        mobileHidden: true,
+        click: function (event, token) {
+            console.log(event);
+            console.log(token);
+        },
+        load: function (event, token) {
+        }
+    }]
 };
+
