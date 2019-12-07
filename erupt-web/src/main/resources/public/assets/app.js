9359c1f22b719372184691d289c4a0b004885757
liyuepeng <liyuepeng>
2019-12-08 00:40:52 +0800

    更新web模块

--- erupt-web/src/main/resources/public/assets/app.js
+++ erupt-web/src/main/resources/public/assets/app.js
@@ -1,11 +1,11 @@
 window.eruptSiteConfig = {
   //global config
-  domain: "http://192.168.1.118:9999",
+  domain: "http://localhost:9999",
   title: "YuePeng Erupt", //通用数据管理框架
   desc: "通用数据管理框架",
   routerReuse: false,
   amapKey: "15bc4df0bd6758645308fa591f439799",
-    // logoPath: "/assets/logo.png",
+  // logoPath: "/assets/logo.png",
   r_tools: [{
     text: "下载",
     icon: "fa-download",
