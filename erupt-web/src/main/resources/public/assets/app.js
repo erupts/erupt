1f2360f83e829f33bbfbd31d6dfce2d0ad05c09e
liyuepeng <liyuepeng>
2020-03-15 22:28:04 +0800

    更新web模块

--- erupt-web/src/main/resources/public/assets/app.js
+++ erupt-web/src/main/resources/public/assets/app.js
@@ -7,17 +7,17 @@ window.eruptSiteConfig = {
     amapKey: "15bc4df0bd6758645308fa591f439799",
     // logoPath: "/assets/logo.png",
     // registerPage:"http://www.baidu.com",
-    // r_tools: [{
-    //     text: "下载",
-    //     icon: "fa-download",
-    //     mobileHidden: true,
-    //     click: function (event, token) {
-    //         console.log(event);
-    //         console.log(token);
-    //     },
-    //     load: function (event, token) {
-    //     }
-    // }]
+    r_tools: [{
+        text: "下载",
+        icon: "fa-download",
+        mobileHidden: true,
+        click: function (event, token) {
+            console.log(event);
+            console.log(token);
+        },
+        load: function (event, token) {
+        }
+    }]
 };
 window.eruptEvent = {
     Router: {
