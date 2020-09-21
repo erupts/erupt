3ff0171896e6a1bec08bc708fc14727f0c30602c
yuepeng <erupts@126.com>
2020-09-21 12:22:39 +0800

    为保证本地数据安全，删除示例，示例代码后期会考虑单独创建项目存放。

--- demo/restart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-name=demo-0.0.1.jar
-pid=`ps -ef | grep ${name}|grep -v grep|awk '{print $2}'`
-for id in $pid
-	do
-	   kill -9 $id
-	done 
-nohup java -jar ./${name} >>./log.txt &
-tail -f ./log.txt
--- demo/src/main/resources/tpl/header.ftl
+++
@@ -1,3 +0,0 @@
-<header style="margin:0 25px;">
-    <h1 style="line-height: 60px;text-align: center">use FreeMarker render</h1>
-</header>
