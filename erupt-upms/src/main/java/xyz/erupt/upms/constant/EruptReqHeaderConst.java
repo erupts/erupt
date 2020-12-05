97ea9aad3d06151aa1e081b16112430b6b04abf7
yuepeng <erupts@126.com>
2020-12-05 18:24:49 +0800

    修复类找不到bug

--- erupt-upms/src/main/java/xyz/erupt/upms/constant/EruptReqHeaderConst.java
+++ erupt-upms/src/main/java/xyz/erupt/upms/constant/EruptReqHeaderConst.java
@@ -4,10 +4,10 @@ public class EruptReqHeaderConst {
 
     public static final String ERUPT_HEADER_KEY = "erupt";
 
+    public static final String ERUPT_HEADER_TOKEN = "token";
+
     //param
     public static final String URL_ERUPT_PARAM_KEY = "_erupt";
-
-    public static final String ERUPT_HEADER_TOKEN = "token";
 
     public static final String URL_ERUPT_PARAM_TOKEN = "_token";
 }
