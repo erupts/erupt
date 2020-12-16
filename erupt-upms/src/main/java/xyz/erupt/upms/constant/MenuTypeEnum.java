fd1ef2b3117a595d6f06365ac71b02d8f6484b34
yuepeng <erupts@126.com>
2020-12-16 18:26:15 +0800

    增加DictChoiceFetchHandler工具类，用于动态获取字典项中的值！可在choice组件fetchHandler中使用

--- erupt-upms/src/main/java/xyz/erupt/upms/constant/MenuTypeEnum.java
+++ erupt-upms/src/main/java/xyz/erupt/upms/constant/MenuTypeEnum.java
@@ -10,7 +10,8 @@ public enum MenuTypeEnum {
     LINK("link", "链接", "互联网地址"),
     NEW_WINDOW("newWindow", "新页签", "互联网地址"),
     FILL("fill", "充满屏幕", "路由地址"),
-    ROUTER("router", "页面路由", "路由地址"),
+    ROUTER("router", "页面路由", null),
+    API("api", "接口名称", null),
     ;
 
     private final String code;
---
+++ erupt-upms/src/main/java/xyz/erupt/upms/handler/DictChoiceFetchHandler.java
@@ -0,0 +1,36 @@
+package xyz.erupt.upms.handler;
+
+import org.springframework.stereotype.Component;
+import xyz.erupt.annotation.fun.ChoiceFetchHandler;
+import xyz.erupt.annotation.fun.VLModel;
+import xyz.erupt.db.dao.EruptDao;
+import xyz.erupt.upms.model.EruptDictItem;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author liyuepeng
+ * @date 2020/12/16 18:00
+ */
+@Component
+public class DictChoiceFetchHandler implements ChoiceFetchHandler {
+
+    @Resource
+    private EruptDao eruptDao;
+
+    @Override
+    public List<VLModel> fetch(String[] params) {
+        if (null == params || params.length == 0) {
+            throw new RuntimeException("DictChoiceFetchHandler → params[0] must dict → code");
+        }
+        List<VLModel> list = new ArrayList<>();
+        List<EruptDictItem> eruptDictItem = eruptDao.queryEntityList(EruptDictItem.class, "eruptDict.code = '" + params[0] + "'", null);
+        for (EruptDictItem item : eruptDictItem) {
+            list.add(new VLModel(item.getId().toString(), item.getName()));
+        }
+        return list;
+    }
+
+}
