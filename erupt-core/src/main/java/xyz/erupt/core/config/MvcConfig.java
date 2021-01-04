bad523ce0060080e40c6e8098ab88af704bd1c4d
hades <lhqblue@outlook.com>
2021-01-04 22:16:45 +0800

    更改Long序列化器，避免前端精度丢失问题， 更改LocalDateTime序列化器、反序列化器， 使得LocalDateTime能正确序列化or反序列化成"yyyy-MM-dd HH:mm:ss"，

--- erupt-core/src/main/java/xyz/erupt/core/config/MvcConfig.java
+++ erupt-core/src/main/java/xyz/erupt/core/config/MvcConfig.java
@@ -1,9 +1,7 @@
 package xyz.erupt.core.config;
 
-import com.google.gson.ExclusionStrategy;
-import com.google.gson.FieldAttributes;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+
+import com.google.gson.*;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,30 +9,46 @@ import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.GsonHttpMessageConverter;
 import xyz.erupt.annotation.config.SkipSerialize;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collection;
+
 
 /**
  * @author liyuepeng
  * @date 10/31/18.
  */
 @Configuration
-public class MvcConfig {
-
+public class MvcConfig  {
+	//序列化
     @Bean
     public HttpMessageConverters customConverters() {
         Collection<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
         GsonHttpMessageConverter gsonHttpMessageConverter = new GsonHttpMessageConverter();
-        Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
+		Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss")
+				.setPrettyPrinting()
+				/* 更改先后顺序没有影响 */
+				.registerTypeAdapter(LocalDateTime.class, (JsonSerializer<LocalDateTime>) (src, typeOfSrc, context) -> new JsonPrimitive(src.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))))
+				.registerTypeAdapter(LocalDate.class, (JsonSerializer<LocalDate>) (src, typeOfSrc, context) -> new JsonPrimitive(src.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))))
+				.registerTypeAdapter(LocalDateTime.class, (JsonDeserializer<LocalDateTime>) (json, type, jsonDeserializationContext) -> {
+					String datetime = json.getAsJsonPrimitive().getAsString();
+					return LocalDateTime.parse(datetime, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+				}).registerTypeAdapter(LocalDate.class, (JsonDeserializer<LocalDate>) (json, type, jsonDeserializationContext) -> {
+					String datetime = json.getAsJsonPrimitive().getAsString();
+					return LocalDate.parse(datetime, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+				})
+				.setLongSerializationPolicy(LongSerializationPolicy.STRING)
+				.create();
         gsonHttpMessageConverter.setGson(gson);
         messageConverters.add(gsonHttpMessageConverter);
         return new HttpMessageConverters(true, messageConverters);
     }
-
-
     @Bean
     public Gson gson() {
-        return new GsonBuilder().setExclusionStrategies(new ExclusionStrategy() {
+        return new GsonBuilder()
+				.setExclusionStrategies(new ExclusionStrategy() {
             @Override
             public boolean shouldSkipField(FieldAttributes f) {
                 SkipSerialize skip = f.getAnnotation(SkipSerialize.class);
@@ -45,7 +59,9 @@ public class MvcConfig {
             public boolean shouldSkipClass(Class<?> incomingClass) {
                 return false;
             }
-        }).setDateFormat("yyyy-MM-dd HH:mm:ss").serializeNulls().create();
+        }).setDateFormat("yyyy-MM-dd HH:mm:ss")
+				.serializeNulls()
+				.create();
     }
 
 }
