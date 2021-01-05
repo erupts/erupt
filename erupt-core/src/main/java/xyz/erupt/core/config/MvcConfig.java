384ab00f64cffeb061e68998955506e1fdc31658
yuepeng <erupts@126.com>
2021-01-05 12:51:37 +0800

    优化已合并代码 MvcConfig

--- erupt-core/src/main/java/xyz/erupt/core/config/MvcConfig.java
+++ erupt-core/src/main/java/xyz/erupt/core/config/MvcConfig.java
@@ -21,34 +21,28 @@ import java.util.Collection;
  * @date 10/31/18.
  */
 @Configuration
-public class MvcConfig  {
-	//序列化
+public class MvcConfig {
+
     @Bean
     public HttpMessageConverters customConverters() {
         Collection<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
         GsonHttpMessageConverter gsonHttpMessageConverter = new GsonHttpMessageConverter();
-		Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss")
-				.setPrettyPrinting()
-				/* 更改先后顺序没有影响 */
-				.registerTypeAdapter(LocalDateTime.class, (JsonSerializer<LocalDateTime>) (src, typeOfSrc, context) -> new JsonPrimitive(src.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))))
-				.registerTypeAdapter(LocalDate.class, (JsonSerializer<LocalDate>) (src, typeOfSrc, context) -> new JsonPrimitive(src.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))))
-				.registerTypeAdapter(LocalDateTime.class, (JsonDeserializer<LocalDateTime>) (json, type, jsonDeserializationContext) -> {
-					String datetime = json.getAsJsonPrimitive().getAsString();
-					return LocalDateTime.parse(datetime, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-				}).registerTypeAdapter(LocalDate.class, (JsonDeserializer<LocalDate>) (json, type, jsonDeserializationContext) -> {
-					String datetime = json.getAsJsonPrimitive().getAsString();
-					return LocalDate.parse(datetime, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-				})
-				.setLongSerializationPolicy(LongSerializationPolicy.STRING)
-				.create();
+        Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss")
+                .registerTypeAdapter(LocalDateTime.class, (JsonDeserializer<LocalDateTime>) (json, type, jsonDeserializationContext)
+                        -> LocalDateTime.parse(json.getAsJsonPrimitive().getAsString(), DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
+                .registerTypeAdapter(LocalDate.class, (JsonDeserializer<LocalDate>) (json, type, jsonDeserializationContext)
+                        -> LocalDate.parse(json.getAsJsonPrimitive().getAsString(), DateTimeFormatter.ofPattern("yyyy-MM-dd")))
+                .setLongSerializationPolicy(LongSerializationPolicy.STRING) //
+                .serializeNulls()
+                .create();
         gsonHttpMessageConverter.setGson(gson);
         messageConverters.add(gsonHttpMessageConverter);
         return new HttpMessageConverters(true, messageConverters);
     }
+
     @Bean
     public Gson gson() {
-        return new GsonBuilder()
-				.setExclusionStrategies(new ExclusionStrategy() {
+        return new GsonBuilder().setExclusionStrategies(new ExclusionStrategy() {
             @Override
             public boolean shouldSkipField(FieldAttributes f) {
                 SkipSerialize skip = f.getAnnotation(SkipSerialize.class);
@@ -59,9 +53,7 @@ public class MvcConfig  {
             public boolean shouldSkipClass(Class<?> incomingClass) {
                 return false;
             }
-        }).setDateFormat("yyyy-MM-dd HH:mm:ss")
-				.serializeNulls()
-				.create();
+        }).setDateFormat("yyyy-MM-dd HH:mm:ss").serializeNulls().create();
     }
 
 }
