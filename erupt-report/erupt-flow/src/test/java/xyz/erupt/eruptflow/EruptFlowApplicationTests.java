f63de3b792c933e067a16c3500d601474a393bf5
yuepeng <erupts@126.com>
2021-01-18 16:07:10 +0800

    初始化erupt-flow流程引擎模块

---
+++ erupt-report/erupt-flow/src/test/java/xyz/erupt/eruptflow/EruptFlowApplicationTests.java
@@ -0,0 +1,13 @@
+//package xyz.erupt.eruptflow;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class EruptFlowApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
