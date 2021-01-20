50439ea66b4f6926cef9d30cf849f56f03197078
yuepeng <erupts@126.com>
2021-01-20 18:32:40 +0800

    1、修复excel导出最多只能500条的bug 2、添加erupt-tpl模块源码 3、添加erupt-generator模块源码 4、更稳erupt-web模块源码，支持路由复用等功能

---
+++ erupt-report/erupt-flow/src/main/java/xyz/erupt/eruptflow/model/HyperFlow.java
@@ -0,0 +1,21 @@
+package xyz.erupt.eruptflow.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import xyz.erupt.annotation.PreDataProxy;
+import xyz.erupt.upms.model.base.HyperModel;
+
+import javax.persistence.MappedSuperclass;
+
+/**
+ * @author liyuepeng
+ * @date 2021/1/18 17:09
+ */
+@Getter
+@Setter
+@MappedSuperclass
+@PreDataProxy(HyperFlowProxy.class)
+public class HyperFlow extends HyperModel {
+
+    private String status;
+}
---
+++ erupt-report/erupt-flow/src/main/java/xyz/erupt/eruptflow/model/HyperFlowProxy.java
@@ -0,0 +1,15 @@
+package xyz.erupt.eruptflow.model;
+
+import xyz.erupt.annotation.fun.DataProxy;
+
+/**
+ * @author liyuepeng
+ * @date 2021/1/18 17:11
+ */
+public class HyperFlowProxy implements DataProxy<HyperFlow> {
+
+    @Override
+    public String beforeFetch() {
+        return null;
+    }
+}
