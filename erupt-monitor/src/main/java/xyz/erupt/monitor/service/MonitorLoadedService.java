98df74db978397869c68de454d3b3808cec1920d
yuepeng <erupts@126.com>
2021-03-02 19:13:41 +0800

    优化代码

--- erupt-monitor/src/main/java/xyz/erupt/monitor/service/MonitorLoadedService.java
+++ erupt-monitor/src/main/java/xyz/erupt/monitor/service/MonitorLoadedService.java
@@ -30,12 +30,12 @@ public class MonitorLoadedService implements CommandLineRunner {
                 String code = "code";
                 EruptMenu eruptMenu = eruptDao.persistIfNotExist(EruptMenu.class, new EruptMenu("monitor", "系统监控", null, null,
                         Integer.valueOf(EruptMenu.OPEN), 10, "fa fa-bullseye", null), "code", "monitor");
-                eruptDao.persistIfNotExist(EruptMenu.class, new EruptMenu(EruptOnline.class.getSimpleName(), "在线用户", MenuTypeEnum.TABLE.getCode(),
-                        EruptOnline.class.getSimpleName(), Integer.valueOf(EruptMenu.OPEN), 10, null, eruptMenu), code, EruptOnline.class.getSimpleName());
                 eruptDao.persistIfNotExist(EruptMenu.class, new EruptMenu("server.html", "服务监控", EruptTplService.TPL,
                         "server.html", Integer.valueOf(EruptMenu.OPEN), 10, null, eruptMenu), code, "server.html");
                 eruptDao.persistIfNotExist(EruptMenu.class, new EruptMenu("redis.html", "缓存监控", EruptTplService.TPL,
                         "redis.html", Integer.valueOf(EruptMenu.OPEN), 20, null, eruptMenu), code, "redis.html");
+                eruptDao.persistIfNotExist(EruptMenu.class, new EruptMenu(EruptOnline.class.getSimpleName(), "在线用户", MenuTypeEnum.TABLE.getCode(),
+                        EruptOnline.class.getSimpleName(), Integer.valueOf(EruptMenu.OPEN), 30, null, eruptMenu), code, EruptOnline.class.getSimpleName());
             }
         });
     }
