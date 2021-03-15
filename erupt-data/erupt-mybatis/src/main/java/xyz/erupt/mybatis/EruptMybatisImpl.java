a4f4729d8e97e1573acbdd9339614c21ef161a08
yuepeng <erupts@126.com>
2021-03-15 11:39:56 +0800

    mybatis的基础实现

--- erupt-data/erupt-mybatis/src/main/java/xyz/erupt/mybatis/EruptMybatisImpl.java
+++ erupt-data/erupt-mybatis/src/main/java/xyz/erupt/mybatis/EruptMybatisImpl.java
@@ -1,14 +1,22 @@
 package xyz.erupt.mybatis;
 
-import tk.mybatis.mapper.mapperhelper.EntityHelper;
-import tk.mybatis.mapper.mapperhelper.MapperHelper;
-import tk.mybatis.mapper.weekend.Weekend;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import lombok.SneakyThrows;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Service;
 import xyz.erupt.core.query.Column;
 import xyz.erupt.core.query.EruptQuery;
 import xyz.erupt.core.service.IEruptDataService;
+import xyz.erupt.core.util.DataProcessorManager;
 import xyz.erupt.core.view.EruptModel;
 import xyz.erupt.core.view.Page;
+import xyz.erupt.mybatis.service.EruptMybatisService;
 
+import java.io.Serializable;
+import java.lang.reflect.Field;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -17,40 +25,57 @@ import java.util.Map;
  * @author liyuepeng
  * @date 2020/12/8 20:49
  */
-public class EruptMybatisImpl implements IEruptDataService {
+@Service
+public class EruptMybatisImpl implements IEruptDataService, ApplicationRunner {
+
     @Override
     public Object findDataById(EruptModel eruptModel, Object id) {
-        Weekend weekend = new Weekend(eruptModel.getClazz());
-        MapperHelper mapperHelper = new MapperHelper();
-        EntityHelper.getEntityTable(eruptModel.getClazz());
-        mapperHelper.registerMapper(eruptModel.getClazz());
-        mapperHelper.getMapperTemplateByMsId("");
-//        weekend.weekendCriteria().byid
-        return null;
+        BaseMapper baseMapper = EruptMybatisService.findMapper(eruptModel.getClazz());
+        return baseMapper.selectById((Serializable) id);
     }
 
     @Override
+    @SneakyThrows
     public Page queryList(EruptModel eruptModel, Page page, EruptQuery eruptQuery) {
-        return null;
+        BaseMapper baseMapper = EruptMybatisService.findMapper(eruptModel.getClazz());
+        com.baomidou.mybatisplus.extension.plugins.pagination.Page.class.newInstance();
+        IPage<?> p = EruptMybatisService.cratePageInstance(eruptModel.getClazz());
+        p.setSize(page.getPageSize());
+        p.setCurrent(page.getPageIndex());
+        //TODO build wrapper
+        QueryWrapper<?> wrapper = new QueryWrapper<>();
+        page.setList(baseMapper.selectMapsPage(p, wrapper).getRecords());
+        page.setTotal(baseMapper.selectCount(wrapper).longValue());
+        return page;
     }
 
     @Override
     public Collection<Map<String, Object>> queryColumn(EruptModel eruptModel, List<Column> columns, EruptQuery eruptQuery) {
+        BaseMapper baseMapper = EruptMybatisService.findMapper(eruptModel.getClazz());
+//        baseMapper.selectb
         return null;
     }
 
     @Override
     public void addData(EruptModel eruptModel, Object object) {
-
+        EruptMybatisService.findMapper(eruptModel.getClazz()).insert(object);
     }
 
     @Override
     public void editData(EruptModel eruptModel, Object object) {
-
+        EruptMybatisService.findMapper(eruptModel.getClazz()).updateById(object);
     }
 
     @Override
+    @SneakyThrows
     public void deleteData(EruptModel eruptModel, Object object) {
+        Field pk = object.getClass().getDeclaredField(eruptModel.getErupt().primaryKeyCol());
+        pk.setAccessible(true);
+        EruptMybatisService.findMapper(eruptModel.getClazz()).deleteById((Serializable) pk.get(object));
+    }
 
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        DataProcessorManager.register(EruptMybatisConst.MYBATIS_PROCESS, this.getClass());
     }
 }
---
+++ erupt-data/erupt-mybatis/src/main/java/xyz/erupt/mybatis/service/EruptMybatisService.java
@@ -0,0 +1,33 @@
+package xyz.erupt.mybatis.service;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import lombok.SneakyThrows;
+import xyz.erupt.core.util.EruptSpringUtil;
+import xyz.erupt.mybatis.annotation.BindMapper;
+
+import java.lang.reflect.ParameterizedType;
+
+/**
+ * @author liyuepeng
+ * @date 2020/12/8 20:49
+ */
+public class EruptMybatisService {
+
+    public static BaseMapper findMapper(Class<?> eruptClass) {
+        BindMapper bindMapper = eruptClass.getAnnotation(BindMapper.class);
+        if (null != bindMapper) {
+            return (BaseMapper<?>) EruptSpringUtil.getBean(bindMapper.value());
+        }
+        throw new RuntimeException("not found BindMapper annotation");
+    }
+
+    @SneakyThrows
+    public static <T> IPage<T> cratePageInstance(Class<T> clazz) {
+        ParameterizedType ptype = (ParameterizedType) Page.class.getGenericSuperclass();
+        Class cla = (Class<T>) ptype.getActualTypeArguments()[0];
+        return (IPage<T>) cla.newInstance();
+    }
+
+}
