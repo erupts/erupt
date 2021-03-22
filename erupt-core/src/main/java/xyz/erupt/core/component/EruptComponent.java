8c6d2c00b3b7234f24bb40010d0c88272ce968d2
yuepeng <erupts@126.com>
2021-03-22 10:33:34 +0800

    update erupt-web source

---
+++ erupt-core/src/main/java/xyz/erupt/core/component/EruptComponent.java
@@ -0,0 +1,13 @@
+package xyz.erupt.core.component;
+
+import java.lang.annotation.Annotation;
+
+/**
+ * @author liyuepeng
+ * @date 2021/3/20 19:40
+ */
+public interface EruptComponent {
+
+    Class<? extends Annotation> bindAnnotation();
+
+}
