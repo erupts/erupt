061517425c0e81a164d60e30f13d0219532ba5a0
yuepeng <erupts@126.com>
2021-03-22 10:31:57 +0800

    update erupt-web source

---
+++ erupt-core/src/main/java/xyz/erupt/core/component/TagsComponent.java
@@ -0,0 +1,18 @@
+package xyz.erupt.core.component;
+
+import xyz.erupt.annotation.sub_field.sub_edit.TagsType;
+
+import java.lang.annotation.Annotation;
+
+/**
+ * @author liyuepeng
+ * @date 2021/3/20 19:40
+ */
+public class TagsComponent implements EruptComponent {
+
+    @Override
+    public Class<? extends Annotation> bindAnnotation() {
+        return TagsType.class;
+    }
+
+}
