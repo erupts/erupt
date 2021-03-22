ec0e0daa8e7ed024b5bd1b03028ad0bc29347c6d
yuepeng <erupts@126.com>
2021-03-22 10:31:57 +0800

    update erupt-web source

---
+++ erupt-annotation/src/main/java/xyz/erupt/annotation/sub_field/ReadOnly.java
@@ -0,0 +1,23 @@
+package xyz.erupt.annotation.sub_field;
+
+import java.beans.Transient;
+
+/**
+ * @author liyuepeng
+ * @date 2021/3/22 10:13
+ */
+public @interface ReadOnly {
+    BehaviorEnum value() default BehaviorEnum.NONE;
+
+    @Transient
+    String[] params() default {};
+
+    @Transient
+    Class<? extends ReadOnly.ReadOnlyHandler> exprHandler() default ReadOnly.ReadOnlyHandler.class;
+
+    interface ReadOnlyHandler {
+
+        BehaviorEnum handler(BehaviorEnum behaviorEnum, String[] params);
+
+    }
+}
--- erupt-core/src/main/java/xyz/erupt/core/controller/EruptComponentController.java
+++ erupt-core/src/main/java/xyz/erupt/core/controller/EruptComponentController.java
@@ -4,13 +4,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import xyz.erupt.annotation.fun.VLModel;
 import xyz.erupt.annotation.sub_field.sub_edit.AutoCompleteType;
 import xyz.erupt.core.annotation.EruptRouter;
 import xyz.erupt.core.constant.EruptRestPath;
-import xyz.erupt.core.exception.EruptNoLegalPowerException;
 import xyz.erupt.core.exception.EruptWebApiRuntimeException;
 import xyz.erupt.core.service.EruptCoreService;
 import xyz.erupt.core.util.EruptSpringUtil;
+import xyz.erupt.core.util.EruptUtil;
 import xyz.erupt.core.view.EruptFieldModel;
 
 import java.util.List;
@@ -29,14 +30,29 @@ public class EruptComponentController {
                                               @PathVariable("field") String field,
                                               @RequestParam("val") String val) {
         EruptFieldModel fieldModel = EruptCoreService.getErupt(eruptName).getEruptFieldMap().get(field);
-        if (null == fieldModel) {
-            throw new EruptNoLegalPowerException();
-        }
         AutoCompleteType autoCompleteType = fieldModel.getEruptField().edit().autoCompleteType();
         if (val.length() < autoCompleteType.triggerLength()) {
             throw new EruptWebApiRuntimeException("char length must >= " + autoCompleteType.triggerLength());
         }
         return EruptSpringUtil.getBean(autoCompleteType.handler()).completeHandler(val, autoCompleteType.param());
+    }
+
+    @RequestMapping("/choice-item/{erupt}/{field}")
+    @EruptRouter(authIndex = 2, verifyType = EruptRouter.VerifyType.ERUPT)
+    public List<VLModel> findChoiceItem(@PathVariable("erupt") String eruptName,
+                                        @PathVariable("field") String field,
+                                        @RequestParam(value = "depend", required = false) String depend) {
+        EruptFieldModel fieldModel = EruptCoreService.getErupt(eruptName).getEruptFieldMap().get(field);
+        return EruptUtil.getChoiceList(fieldModel.getEruptField().edit().choiceType());
+    }
+
+    @RequestMapping("/tags-item/{erupt}/{field}")
+    @EruptRouter(authIndex = 2, verifyType = EruptRouter.VerifyType.ERUPT)
+    public List<String> findTagsItem(@PathVariable("erupt") String eruptName,
+                                     @PathVariable("field") String field,
+                                     @RequestParam(value = "depend", required = false) String depend) {
+        EruptFieldModel fieldModel = EruptCoreService.getErupt(eruptName).getEruptFieldMap().get(field);
+        return EruptUtil.getTagList(fieldModel.getEruptField().edit().tagsType());
     }
 
 }
