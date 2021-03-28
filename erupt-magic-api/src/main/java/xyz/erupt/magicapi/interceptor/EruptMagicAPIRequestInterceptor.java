d0455d6f8253df63a8ae2c7996a678b98afc12b2
yuepeng <erupts@126.com>
2021-03-28 17:57:04 +0800

    1、修改magic-api适配代码 2、loaded文件内容增加版本号，多版本验证时使用

--- erupt-magic-api/src/main/java/xyz/erupt/magicapi/interceptor/EruptMagicAPIRequestInterceptor.java
+++ erupt-magic-api/src/main/java/xyz/erupt/magicapi/interceptor/EruptMagicAPIRequestInterceptor.java
@@ -10,6 +10,7 @@ import org.ssssssss.magicapi.model.ApiInfo;
 import org.ssssssss.magicapi.model.JsonBean;
 import org.ssssssss.magicapi.model.Options;
 import org.ssssssss.script.MagicScriptContext;
+import xyz.erupt.magicapi.service.MagicAPIDataLoadService;
 import xyz.erupt.upms.model.EruptUser;
 import xyz.erupt.upms.service.EruptUserService;
 
@@ -23,53 +24,53 @@ import java.util.Objects;
 @Component
 public class EruptMagicAPIRequestInterceptor implements RequestInterceptor, AuthorizationInterceptor {
 
+    private final EruptUserService eruptUserService;
 
-	private final EruptUserService eruptUserService;
+    public EruptMagicAPIRequestInterceptor(EruptUserService eruptUserService) {
+        this.eruptUserService = eruptUserService;
+    }
 
-	public EruptMagicAPIRequestInterceptor(EruptUserService eruptUserService) {
-		this.eruptUserService = eruptUserService;
-	}
+    /**
+     * 配置UI界面不需要登录框
+     */
+    @Override
+    public boolean requireLogin() {
+        return false;
+    }
 
-	/**
-	 * 配置UI界面不需要登录框
-	 */
-	@Override
-	public boolean requireLogin() {
-		return false;
-	}
+    /**
+     * 配置接口权限
+     */
+    @Override
+    public Object preHandle(ApiInfo info, MagicScriptContext context, HttpServletRequest request, HttpServletResponse response) {
+        String permission = Objects.toString(info.getOptionValue(Options.PERMISSION), "");
+        String role = Objects.toString(info.getOptionValue(Options.ROLE), "");
+        if (StringUtils.isNotBlank(permission) || StringUtils.isNotBlank(role)) {
+            EruptUser user = eruptUserService.getCurrentEruptUser();
+            // 未登录
+            if (user == null) {
+                return new JsonBean<Void>(401, "用户未登录");
+            } else {
+                // 权限判断
+                if (StringUtils.isNotBlank(permission) && eruptUserService.getEruptMenuByValue(permission) == null) {
+                    return new JsonBean<Void>(403, "用户权限不足");
+                }
+                // 角色判断
+                if (StringUtils.isNotBlank(role) && user.getRoles().stream().anyMatch(it -> role.equals(it.getCode()))) {
+                    return new JsonBean<Void>(403, "用户权限不足");
+                }
+            }
+        }
+        return null;
+    }
 
-	/**
-	 * 配置接口权限
-	 */
-	@Override
-	public Object preHandle(ApiInfo info, MagicScriptContext context, HttpServletRequest request, HttpServletResponse response) {
-		String permission = Objects.toString(info.getOptionValue(Options.PERMISSION),"");
-		String role = Objects.toString(info.getOptionValue(Options.ROLE),"");
-		if(StringUtils.isNotBlank(permission) || StringUtils.isNotBlank(role)){
-			EruptUser user = eruptUserService.getCurrentEruptUser();
-			// 未登录
-			if(user == null){
-				return new JsonBean<Void>(401,"用户未登录");
-			}else {
-				// 权限判断
-				if(StringUtils.isNotBlank(permission) && eruptUserService.getEruptMenuByValue(permission) == null){
-					return new JsonBean<Void>(403,"用户权限不足");
-				}
-				// 角色判断
-				if(StringUtils.isNotBlank(role) && user.getRoles().stream().anyMatch(it -> role.equals(it.getCode()))){
-					return new JsonBean<Void>(403,"用户权限不足");
-				}
-			}
-		}
-		return null;
-	}
-
-	/**
-	 * 配置UI鉴权
-	 */
-	@Override
-	public boolean allowVisit(MagicUser magicUser, HttpServletRequest request, Authorization authorization) {
-		// 未登录或UI权限不足
-		return eruptUserService.getCurrentEruptUser() != null && eruptUserService.getEruptMenuByValue(authorization.name()) != null;
-	}
+    /**
+     * 配置UI鉴权
+     */
+    @Override
+    public boolean allowVisit(MagicUser magicUser, HttpServletRequest request, Authorization authorization) {
+        // 未登录或UI权限不足
+        return eruptUserService.getCurrentUid() != null
+                && eruptUserService.getEruptMenuByValue(MagicAPIDataLoadService.MAGIC_API_MENU_PREFIX + authorization.name()) != null;
+    }
 }
