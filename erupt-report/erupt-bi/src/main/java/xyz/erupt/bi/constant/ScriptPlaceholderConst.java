84934363ce2619b6b7db4a91db57fafd9452c54a
yuepeng <erupts@126.com>
2021-04-20 18:41:02 +0800

    优化erupt-bi模块默认变量的命名规范

--- erupt-report/erupt-bi/src/main/java/xyz/erupt/bi/constant/ScriptPlaceholderConst.java
+++ erupt-report/erupt-bi/src/main/java/xyz/erupt/bi/constant/ScriptPlaceholderConst.java
@@ -7,12 +7,12 @@ package xyz.erupt.bi.constant;
 public class ScriptPlaceholderConst {
 
     //导出标识符
-    public static final String EXPORT_PLACEHOLDER = "$export";
+    public static final String EXPORT_PLACEHOLDER = "__export__";
 
     //当前登录用户ID
-    public static final String USER_ID_PLACEHOLDER = "$uid";
+    public static final String USER_ID_PLACEHOLDER = "__uid__";
 
     //request 请求对象
-    public static final String REQUEST_PLACEHOLDER = "$request";
+    public static final String REQUEST_PLACEHOLDER = "__request__";
 
 }
