bed6de74cef289d6d18e2ea22793f732bac0466f
yuepeng <erupts@126.com>
2021-04-27 22:48:20 +0800

    1、升级版本号至1.7.0-bate 2、使用spring.factories管理包 3、简化入口类配置

--- erupt-core/src/main/java/xyz/erupt/core/EruptCoreAutoConfiguration.java
+++ erupt-core/src/main/java/xyz/erupt/core/EruptCoreAutoConfiguration.java
@@ -1,18 +1,16 @@
-//package xyz.erupt.core;
-//
-//import org.springframework.boot.autoconfigure.domain.EntityScan;
-//import org.springframework.boot.context.properties.EnableConfigurationProperties;
-//import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.context.annotation.Configuration;
-//import xyz.erupt.core.config.EruptAppProp;
-//import xyz.erupt.core.config.EruptProp;
-//
-///**
-// * @author YuePeng
-// * date 2021/3/28 18:51
-// */
-//@Configuration
-//@ComponentScan
-//@EntityScan
-//public class EruptCoreAutoConfiguration {
-//}
+package xyz.erupt.core;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import xyz.erupt.core.config.EruptProp;
+
+/**
+ * @author YuePeng
+ * date 2021/3/28 18:51
+ */
+@Configuration
+@ComponentScan
+@EnableConfigurationProperties(EruptProp.class)
+public class EruptCoreAutoConfiguration {
+}
---
+++ erupt-security/src/main/java/xyz/erupt/security/EruptSecurityAutoConfiguration.java
@@ -0,0 +1,13 @@
+package xyz.erupt.security;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author YuePeng
+ * date 2021/3/28 18:51
+ */
+@Configuration
+@ComponentScan
+public class EruptSecurityAutoConfiguration {
+}
