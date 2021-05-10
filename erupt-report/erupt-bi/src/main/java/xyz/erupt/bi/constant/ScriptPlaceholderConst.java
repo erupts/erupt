0e9e3250cede28f994c85cabedb3d1a64ca8299e
yuepeng <erupts@126.com>
2021-05-10 16:50:35 +0800

    🧩 ScriptPlaceholderConst

--- erupt-report/erupt-bi/src/main/java/xyz/erupt/bi/constant/ScriptPlaceholderConst.java
+++ erupt-report/erupt-bi/src/main/java/xyz/erupt/bi/constant/ScriptPlaceholderConst.java
@@ -6,16 +6,20 @@ package xyz.erupt.bi.constant;
  */
 public class ScriptPlaceholderConst {
 
+    private static final String SYMBOL = "{}";
+
+    private static final String PLACEHOLDER = "__" + SYMBOL + "__";
+
     //导出标识符
-    public static final String EXPORT_PLACEHOLDER = "__export__";
+    public static final String EXPORT_PLACEHOLDER = PLACEHOLDER.replace(SYMBOL, "export");
 
     //当前登录用户ID
-    public static final String USER_ID_PLACEHOLDER = "__uid__";
+    public static final String USER_ID_PLACEHOLDER = PLACEHOLDER.replace(SYMBOL, "uid");
 
     //request 请求对象
-    public static final String REQUEST_PLACEHOLDER = "__request__";
+    public static final String REQUEST_PLACEHOLDER = PLACEHOLDER.replace(SYMBOL, "request");
 
-    //request 请求对象
-    public static final String RESPONSE_PLACEHOLDER = "__response__";
+    //response 响应对象
+    public static final String RESPONSE_PLACEHOLDER = PLACEHOLDER.replace(SYMBOL, "response");
 
 }
