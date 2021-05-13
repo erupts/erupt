75212bf85d5adb6d520f4207b103168e0013c63f
yuepeng <erupts@126.com>
2021-05-13 18:00:10 +0800

    update README.md

--- erupt-core/src/main/java/xyz/erupt/core/service/EruptExcelService.java
+++ erupt-core/src/main/java/xyz/erupt/core/service/EruptExcelService.java
@@ -71,6 +71,9 @@ public class EruptExcelService {
                     Cell cell = row.createCell(colNum);
                     cell.setCellStyle(headStyle);
                     cell.setCellValue(view.title());
+//                    if (StringUtils.isNotBlank(view.width())) {
+//                        sheet.setColumnWidth(colNum, view.width());
+//                    }
                     colNum++;
                 }
             }
