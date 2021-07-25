9b6e87f4752ed0d7bdee3db5741cf3f6a971b16d
yuepeng <erupts@126.com>
2021-07-25 12:53:51 +0800

    非管理员用户管理角色时可根据已有角色菜单向下分配，无权分配没有权限的菜单，实现任何登录用户都可以无限向下分配子用户的能力 1、角色支持根据已登录用户身份自动过滤 2、角色菜单可根据已登录用户身份自动过滤

---
+++ erupt-upms/src/main/java/xyz/erupt/upms/handler/RoleMenuFilter.java
@@ -0,0 +1,39 @@
+package xyz.erupt.upms.handler;
+
+import org.springframework.stereotype.Service;
+import xyz.erupt.annotation.fun.FilterHandler;
+import xyz.erupt.upms.model.EruptMenu;
+import xyz.erupt.upms.model.EruptRole;
+import xyz.erupt.upms.model.EruptUser;
+import xyz.erupt.upms.service.EruptUserService;
+
+import javax.annotation.Resource;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * @author YuePeng
+ * date 2021/7/25 12:25
+ */
+@Service
+public class RoleMenuFilter implements FilterHandler {
+
+    @Resource
+    private EruptUserService eruptUserService;
+
+    @Override
+    public String filter(String condition, String[] params) {
+        EruptUser eruptUser = eruptUserService.getCurrentEruptUser();
+        if (eruptUser.getIsAdmin()) {
+            return null;
+        } else {
+            Set<EruptMenu> menuSet = new HashSet<>();
+            eruptUser.getRoles().stream().filter(EruptRole::getStatus).map(EruptRole::getMenus).forEach(menuSet::addAll);
+            List<String> menus = menuSet.stream().map(it -> it.getId().toString()).collect(Collectors.toList());
+            return String.format("EruptMenu.id in (%s)", String.join(",", String.join(",", menus)));
+        }
+    }
+
+}
