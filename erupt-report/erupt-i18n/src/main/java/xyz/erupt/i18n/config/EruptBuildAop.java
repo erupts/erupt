684bc3b1dbf87b1995ee72359bbcfec1f8a268bd
yuepeng <erupts@126.com>
2021-09-13 01:02:19 +0800

    add i18n code

--- erupt-report/erupt-i18n/src/main/java/xyz/erupt/i18n/config/EruptBuildAop.java
+++ erupt-report/erupt-i18n/src/main/java/xyz/erupt/i18n/config/EruptBuildAop.java
@@ -1,22 +1,49 @@
 package xyz.erupt.i18n.config;
 
-import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import xyz.erupt.core.view.EruptBuildModel;
+import xyz.erupt.core.view.EruptFieldModel;
+import xyz.erupt.i18n.constant.I18nConstant;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author YuePeng
  * date 2021/9/12 23:55
  */
+@Aspect
+@Component
 public class EruptBuildAop {
 
-    @Pointcut("execution(public * xyz.erupt.core.controller.EruptBuildController.getEruptBuild(..))")
-    public void cut() {
+    private final String POINT_CUT = "execution(public * xyz.erupt.core.controller.EruptBuildController.getEruptBuild(..))";
+    @Resource
+    private HttpServletRequest request;
+
+    @AfterReturning(returning = "eruptBuildModel", pointcut = POINT_CUT)
+    public void doAfterReturning(EruptBuildModel eruptBuildModel) {
+        for (EruptFieldModel fieldModel : eruptBuildModel.getEruptModel().getEruptFieldModels()) {
+            eruptFieldJsonI18n(fieldModel.getEruptFieldJson());
+        }
     }
 
-    @AfterReturning("cut()")
-    public void doAfterReturning(Object obj) {
-        System.out.println(new Gson().toJson(obj));
+    private void eruptFieldJsonI18n(JsonObject eruptFieldJson) {
+        if (eruptFieldJson.has(I18nConstant.EDIT)) {
+            eruptFieldJson.getAsJsonObject(I18nConstant.EDIT).addProperty(I18nConstant.TITLE, 1);
+        }
+        if (eruptFieldJson.has(I18nConstant.VIEWS)) {
+            for (JsonElement element : eruptFieldJson.getAsJsonArray(I18nConstant.VIEWS)) {
+                element.getAsJsonObject().addProperty(I18nConstant.TITLE, 1);
+            }
+        }
+    }
+
+    private void eruptJsonI18n(JsonObject eruptJson) {
+
     }
 
 }
---
+++ erupt-report/erupt-i18n/src/main/java/xyz/erupt/i18n/constant/I18nConstant.java
@@ -0,0 +1,17 @@
+package xyz.erupt.i18n.constant;
+
+/**
+ * @author YuePeng
+ * date 2021/9/13 00:44
+ */
+public class I18nConstant {
+
+    public static final String VIEWS = "views";
+
+    public static final String VIEW = "view";
+
+    public static final String EDIT = "edit";
+
+    public static final String TITLE = "title";
+
+}
