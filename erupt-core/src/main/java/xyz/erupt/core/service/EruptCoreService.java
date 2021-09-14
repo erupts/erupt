49a580afef970d5a6f421983997647eb7db183aa
yuepeng <erupts@126.com>
2021-09-15 00:01:47 +0800

    修复根据权限显示自定义按钮报错的bug

--- erupt-core/src/main/java/xyz/erupt/core/service/EruptCoreService.java
+++ erupt-core/src/main/java/xyz/erupt/core/service/EruptCoreService.java
@@ -45,7 +45,7 @@ public class EruptCoreService implements ApplicationRunner {
         return ERUPTS.get(eruptName);
     }
 
-    //需要动态构建的EruptModel对象属性值，通过此方法暴露给外界使用
+    //需要动态构建的EruptModel视图属性
     @SneakyThrows
     public static EruptModel getEruptView(String eruptName) {
         EruptModel em = getErupt(eruptName).clone();
