7dd107366cb174172f85012e3cf8a13496a37c0a
yuepeng <erupts@126.com>
2021-09-18 00:41:56 +0800

    upgrade to 1.8.5

--- erupt-report/erupt-i18n/src/main/java/xyz/erupt/i18n/annotation/annotation.java
+++
@@ -1,8 +0,0 @@
-package xyz.erupt.i18n.annotation;
-
-/**
- * @author YuePeng
- * date 2021/9/1 00:11
- */
-public class annotation {
-}
--- erupt-report/erupt-i18n/src/main/java/xyz/erupt/i18n/config/EruptBuildAop.java
+++ erupt-report/erupt-i18n/src/main/java/xyz/erupt/i18n/config/EruptBuildAop.java
@@ -40,7 +40,7 @@ public class EruptBuildAop {
 
     @AfterReturning(pointcut = POINT_CUT, returning = "eruptBuildModel")
     public void doAfterReturning(EruptBuildModel eruptBuildModel) {
-        if (!StringUtils.isNotBlank(request.getHeader(LANG_HEADER))) {
+        if (StringUtils.isNotBlank(request.getHeader(LANG_HEADER))) {
             Optional.ofNullable(eruptContextService.getContextEruptClass()).ifPresent(eruptClass -> {
                 EruptI18n eruptI18n = eruptClass.getAnnotation(EruptI18n.class);
                 if (null != eruptI18n && eruptI18n.enable()) {
