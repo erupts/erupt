916bbc9edde94defaf397e623bce6029568447d1
yuepeng <erupts@126.com>
2021-09-18 00:15:33 +0800

    优化代码质量

--- erupt-core/src/main/java/xyz/erupt/core/invoke/DataProxyInvoke.java
+++ erupt-core/src/main/java/xyz/erupt/core/invoke/DataProxyInvoke.java
@@ -16,22 +16,22 @@ import java.util.stream.Stream;
  */
 public class DataProxyInvoke {
 
-    public static void invoke(EruptModel eruptModel, Consumer<DataProxy> consumer) {
+    public static void invoke(EruptModel eruptModel, Consumer<DataProxy<Object>> consumer) {
         //父类及接口 @PreDataProxy
         ReflectUtil.findClassExtendStack(eruptModel.getClazz()).forEach(clazz -> DataProxyInvoke.actionInvokePreDataProxy(clazz, consumer));
         //本类及接口 @PreDataProxy
         DataProxyInvoke.actionInvokePreDataProxy(eruptModel.getClazz(), consumer);
         //@Erupt → DataProxy
-        Stream.of(eruptModel.getErupt().dataProxy()).forEach(proxy -> consumer.accept(EruptSpringUtil.getBean(proxy)));
+        Stream.of(eruptModel.getErupt().dataProxy()).forEach(proxy -> consumer.accept((DataProxy<Object>) EruptSpringUtil.getBean(proxy)));
     }
 
-    private static void actionInvokePreDataProxy(Class<?> clazz, Consumer<DataProxy> consumer) {
+    private static void actionInvokePreDataProxy(Class<?> clazz, Consumer<DataProxy<Object>> consumer) {
         //接口
         Stream.of(clazz.getInterfaces()).forEach(it -> Optional.ofNullable(it.getAnnotation(PreDataProxy.class))
-                .ifPresent(preDataProxy -> consumer.accept(EruptSpringUtil.getBean(preDataProxy.value()))));
+                .ifPresent(preDataProxy -> consumer.accept((DataProxy<Object>) EruptSpringUtil.getBean(preDataProxy.value()))));
         //类
         Optional.ofNullable(clazz.getAnnotation(PreDataProxy.class))
-                .ifPresent(preDataProxy -> consumer.accept(EruptSpringUtil.getBean(preDataProxy.value())));
+                .ifPresent(preDataProxy -> consumer.accept((DataProxy<Object>) EruptSpringUtil.getBean(preDataProxy.value())));
     }
 
 }
