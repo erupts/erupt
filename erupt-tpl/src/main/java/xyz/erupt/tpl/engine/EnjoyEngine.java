eab3f8b40d2f367bea7985c878b7ec21195b6997
icefairy <860668820@qq.com>
2021-10-27 16:32:47 +0800

    add JFinalEnjoy support

---
+++ erupt-tpl/src/main/java/xyz/erupt/tpl/engine/EnjoyEngine.java
@@ -0,0 +1,32 @@
+package xyz.erupt.tpl.engine;
+
+import com.jfinal.kit.StrKit;
+import com.jfinal.template.Engine;
+import xyz.erupt.annotation.sub_erupt.Tpl;
+
+import java.io.Writer;
+import java.util.Map;
+
+/**
+ * author: icefairy
+ */
+public class EnjoyEngine extends EngineTemplate<Engine> {
+    @Override
+    public Tpl.Engine engine() {
+        return Tpl.Engine.Enjoy;
+    }
+
+    @Override
+    public Engine init() {
+        Engine eng=new Engine();
+        eng.addSharedMethod(StrKit.class);
+        eng.setDevMode(true);
+        setEngine(new Engine());
+        return getEngine();
+    }
+
+    @Override
+    public void render(Engine engine, String filePath, Map<String, Object> bindingMap, Writer out) {
+        engine.getTemplate(filePath).render(bindingMap,out);
+    }
+}
