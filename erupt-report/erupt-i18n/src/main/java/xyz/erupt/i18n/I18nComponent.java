ae842193c58dc76d9609625d9469ae71a50efa13
yuepeng <erupts@126.com>
2021-10-28 17:35:27 +0800

    update

---
+++ erupt-report/erupt-i18n/src/main/java/xyz/erupt/i18n/I18nComponent.java
@@ -0,0 +1,13 @@
+package xyz.erupt.i18n;
+
+/**
+ * @author YuePeng
+ * date 2021/10/28 17:34
+ */
+public class I18nComponent {
+
+    public String fanyi(String key) {
+        return key;
+    }
+
+}
