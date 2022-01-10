05f6dc16ae86c76205b71dd1e11be2340b8501f9
yuepeng <erupts@126.com>
2022-01-11 01:18:33 +0800

    upgrade version

--- erupt-report/maven-versions.sh
+++ erupt-report/maven-versions.sh
@@ -1,10 +1,10 @@
-#cd ~/git/erupt/
-#mvn -DskipTests=true org.codehaus.mojo:versions-maven-plugin:2.7:set
-#mvn -DskipTests=true install
-#
-#cd ~/git/erupt-pro/
-#mvn -DskipTests=true org.codehaus.mojo:versions-maven-plugin:2.7:set
-#mvn -DskipTests=true install
+cd ~/git/erupt/
+mvn -DskipTests=true org.codehaus.mojo:versions-maven-plugin:2.7:set
+mvn -DskipTests=true install
+
+cd ~/git/erupt-pro/
+mvn -DskipTests=true org.codehaus.mojo:versions-maven-plugin:2.7:set
+mvn -DskipTests=true install
 
 cd ~/git/erupt-cloud/
 mvn -DskipTests=true org.codehaus.mojo:versions-maven-plugin:2.7:set
