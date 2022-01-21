db0babaaac285db672369c532c036a0e97e653db
yuepeng <erupts@126.com>
2022-01-21 22:13:45 +0800

    update

--- erupt-core/src/main/java/xyz/erupt/core/controller/EruptWebController.java
+++ erupt-core/src/main/java/xyz/erupt/core/controller/EruptWebController.java
@@ -1,5 +1,6 @@
 package xyz.erupt.core.controller;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import xyz.erupt.core.util.EruptPropUtil;
@@ -11,9 +12,16 @@ import xyz.erupt.core.util.EruptPropUtil;
 @Controller
 public class EruptWebController {
 
+    public static final String RANDOM = RandomStringUtils.randomAlphanumeric(8);
+
     @RequestMapping({"/"})
     public String index() {
-        return "forward:index.html?_v=" + EruptPropUtil.getEruptVersion();
+        return "forward:index.html?v=" + EruptPropUtil.getEruptVersion();
     }
+
+//    @RequestMapping({"app.js", "app.css", "app.module.js"})
+//    public String forward(HttpServletRequest request) {
+//        return request.getServletPath().replace("/", "") + "?_v=" + RANDOM;
+//    }
 
 }
