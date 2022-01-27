2a3934c4ffabde1611cfdc4d6fc7738272677663
yuepeng <erupts@126.com>
2022-01-27 21:49:17 +0800

    移除无效代码

--- erupt-core/src/main/java/xyz/erupt/core/controller/EruptWebController.java
+++ erupt-core/src/main/java/xyz/erupt/core/controller/EruptWebController.java
@@ -1,6 +1,5 @@
 package xyz.erupt.core.controller;
 
-import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import xyz.erupt.core.util.EruptPropUtil;
@@ -12,16 +11,9 @@ import xyz.erupt.core.util.EruptPropUtil;
 @Controller
 public class EruptWebController {
 
-    public static final String RANDOM = RandomStringUtils.randomAlphanumeric(8);
-
     @RequestMapping({"/"})
     public String index() {
         return "forward:index.html?v=" + EruptPropUtil.getEruptVersion();
     }
-
-//    @RequestMapping({"app.js", "app.css", "app.module.js"})
-//    public String forward(HttpServletRequest request) {
-//        return request.getServletPath().replace("/", "") + "?_v=" + RANDOM;
-//    }
 
 }
