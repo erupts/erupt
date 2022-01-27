cd570dac904625f3103661dde2757ebec8d2654d
YuePeng <yuepeng.li@ideamake.cn>
2022-01-27 17:36:38 +0800

    优化报错文案

--- erupt-core/src/main/java/xyz/erupt/core/exception/EruptFieldAnnotationException.java
+++ erupt-core/src/main/java/xyz/erupt/core/exception/EruptFieldAnnotationException.java
@@ -22,7 +22,7 @@ public class EruptFieldAnnotationException extends RuntimeException {
                 if (eruptFieldModel.getEruptField().views().length > 0) {
                     for (View view : eruptFieldModel.getEruptField().views()) {
                         if ("".equals(view.column())) {
-                            throw ExceptionAnsi.styleEruptFieldException(eruptFieldModel, "@View注解" + view.title() + "必须指定column值");
+                            throw ExceptionAnsi.styleEruptFieldException(eruptFieldModel, "@View注解修饰复杂对象 '" + view.title() + "' 必须配置column值");
                         }
                     }
                 }
