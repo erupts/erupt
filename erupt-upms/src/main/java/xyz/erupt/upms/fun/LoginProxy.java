45406196d6ff4f09ded412ed464343d8c024a393
yuepeng <erupts@126.com>
2022-03-26 16:09:03 +0800

    1、优化LoginProxy调用细节 2、优化自定义SQL数据源的使用方式

--- erupt-upms/src/main/java/xyz/erupt/upms/fun/LoginProxy.java
+++ erupt-upms/src/main/java/xyz/erupt/upms/fun/LoginProxy.java
@@ -2,6 +2,7 @@ package xyz.erupt.upms.fun;
 
 import xyz.erupt.annotation.config.Comment;
 import xyz.erupt.core.util.EruptSpringUtil;
+import xyz.erupt.upms.base.LoginModel;
 import xyz.erupt.upms.model.EruptUser;
 import xyz.erupt.upms.service.EruptUserService;
 
@@ -16,7 +17,12 @@ public interface LoginProxy {
     @Comment("Calendar.DAY_OF_MONTH → Calendar.getInstance().get(Calendar.DAY_OF_MONTH) //今天月的哪一天")
     @Comment("如果不希望加密，请前往配置文件，将：erupt-app.pwdTransferEncrypt 设置为 false 即可")
     default EruptUser login(String account, String pwd) {
-        return EruptSpringUtil.getBean(EruptUserService.class).login(account, pwd).getEruptUser();
+        LoginModel loginModel = EruptSpringUtil.getBean(EruptUserService.class).login(account, pwd);
+        if (loginModel.isPass()) {
+            return loginModel.getEruptUser();
+        } else {
+            throw new RuntimeException(loginModel.getReason());
+        }
     }
 
     @Comment("登录成功")
