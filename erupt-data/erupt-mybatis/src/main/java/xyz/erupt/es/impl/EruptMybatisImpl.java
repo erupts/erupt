c8f517ed08526b8de8728235fbc046ddfc031c32
yuepeng <erupts@126.com>
2022-04-29 01:14:48 +0800

    优化mybatis mapper使用流程

--- erupt-data/erupt-mybatis/src/main/java/xyz/erupt/es/impl/EruptMybatisImpl.java
+++ erupt-data/erupt-mybatis/src/main/java/xyz/erupt/es/impl/EruptMybatisImpl.java
@@ -3,19 +3,22 @@ package xyz.erupt.es.impl;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import lombok.SneakyThrows;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Service;
 import xyz.erupt.annotation.query.Condition;
 import xyz.erupt.core.invoke.DataProcessorManager;
 import xyz.erupt.core.query.Column;
 import xyz.erupt.core.query.EruptQuery;
 import xyz.erupt.core.service.IEruptDataService;
-import xyz.erupt.core.util.EruptSpringUtil;
 import xyz.erupt.core.view.EruptModel;
 import xyz.erupt.core.view.Page;
-import xyz.erupt.es.annotation.EruptMybatisORM;
 
+import javax.annotation.Resource;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -29,16 +32,20 @@ public class EruptMybatisImpl implements IEruptDataService {
 
     public static final String MYBATIS_PROCESS = "mybatis";
 
+    @Resource
+    private SqlSessionFactory sqlSessionFactory;
+
+    @Resource
+    private SqlSessionTemplate sqlSessionTemplate;
+
     static {
         DataProcessorManager.register(MYBATIS_PROCESS, EruptMybatisImpl.class);
     }
 
-    private BaseMapper getMapper(EruptModel eruptModel) {
-        EruptMybatisORM eruptMybatisORM = eruptModel.getClazz().getAnnotation(EruptMybatisORM.class);
-        if (null == eruptMybatisORM) {
-            throw new RuntimeException("@" + EruptMybatisORM.class.getSimpleName() + " not found");
-        }
-        return EruptSpringUtil.getBean(eruptMybatisORM.mapper());
+    @SneakyThrows
+    private BaseMapper<Object> getMapper(EruptModel eruptModel) {
+//        return sqlSessionTemplate.getMapper(eruptModel.getClazz());
+        return (BaseMapper<Object>) sqlSessionFactory.openSession().getMapper(eruptModel.getClazz());
     }
 
     @Override
@@ -49,19 +56,19 @@ public class EruptMybatisImpl implements IEruptDataService {
     @SneakyThrows
     @Override
     public Page queryList(EruptModel eruptModel, Page page, EruptQuery eruptQuery) {
-        IPage p = new com.baomidou.mybatisplus.extension.plugins.pagination.Page<>();
+        IPage<Object> p = new com.baomidou.mybatisplus.extension.plugins.pagination.Page<>();
         p.setSize(page.getPageSize());
         p.setCurrent(page.getPageIndex());
-        QueryWrapper wrapper = new QueryWrapper<>();
+        QueryWrapper<Object> wrapper = new QueryWrapper<>();
         this.addQueryCondition(eruptQuery, wrapper);
         this.getMapper(eruptModel).selectPage(p, wrapper);
         page.setTotal(p.getTotal());
         page.setSort(page.getSort());
-        page.setList(p.getRecords());
+        page.setList(null);
         return page;
     }
 
-    public void addQueryCondition(EruptQuery eruptQuery, QueryWrapper queryWrapper) {
+    public void addQueryCondition(EruptQuery eruptQuery, QueryWrapper<?> queryWrapper) {
         for (Condition condition : eruptQuery.getConditions()) {
             switch (condition.getExpression()) {
                 case EQ:
@@ -80,7 +87,6 @@ public class EruptMybatisImpl implements IEruptDataService {
         }
     }
 
-
     @Override
     public void addData(EruptModel eruptModel, Object object) {
         this.getMapper(eruptModel).insert(object);
@@ -93,16 +99,20 @@ public class EruptMybatisImpl implements IEruptDataService {
 
     @Override
     public void deleteData(EruptModel eruptModel, Object object) {
-        this.getMapper(eruptModel).deleteById((Serializable) object);
+        this.getMapper(eruptModel).deleteById(object.toString());
     }
 
     @Override
     public Collection<Map<String, Object>> queryColumn(EruptModel eruptModel, List<Column> columns, EruptQuery eruptQuery) {
-        QueryWrapper wrapper = new QueryWrapper<>();
-//        wrapper.select(columns.stream().map(Column::getName).collect(Collectors.toList()).toArray());
-        this.addQueryCondition(eruptQuery, wrapper);
-        List list = this.getMapper(eruptModel).selectList(wrapper);
-        return list;
+        QueryWrapper<Object> queryWrapper = Wrappers.query(eruptModel.getClazz());
+        List<String> list = new ArrayList<>();
+        for (Column column : columns) {
+            list.add(column.getName());
+        }
+        queryWrapper.select(list.toArray(new String[]{}));
+
+        this.getMapper(eruptModel).selectList(queryWrapper);
+        return null;
     }
 
 }
