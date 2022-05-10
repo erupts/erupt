93238d6f7a09f7791ad68b67ba2f6f20193794ee
yuepeng <erupts@126.com>
2022-05-11 00:24:08 +0800

    upgrade to 1.10.8-beta.2

--- erupt-report/maven-versions.sh
+++ erupt-report/maven-versions.sh
@@ -5,7 +5,3 @@ mvn -DskipTests=true install
 cd ~/git/erupt-pro/
 mvn -DskipTests=true org.codehaus.mojo:versions-maven-plugin:2.7:set
 mvn -DskipTests=true install
-
-cd ~/git/erupt-cloud/
-mvn -DskipTests=true org.codehaus.mojo:versions-maven-plugin:2.7:set
-mvn -DskipTests=true install
