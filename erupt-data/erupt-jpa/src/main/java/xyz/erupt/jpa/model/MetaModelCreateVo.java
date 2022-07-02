27482d63894cadeeacc0314d84cebe275ae6764a
yuepeng <erupts@126.com>
2022-07-02 23:54:38 +0800

    增加MetaModelCreateVo

---
+++ erupt-data/erupt-jpa/src/main/java/xyz/erupt/jpa/model/MetaModelCreateVo.java
@@ -0,0 +1,38 @@
+package xyz.erupt.jpa.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import xyz.erupt.annotation.EruptField;
+import xyz.erupt.annotation.config.EruptSmartSkipSerialize;
+import xyz.erupt.annotation.sub_field.Edit;
+import xyz.erupt.annotation.sub_field.Readonly;
+import xyz.erupt.annotation.sub_field.View;
+import xyz.erupt.annotation.sub_field.sub_edit.DateType;
+
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+/**
+ * @author YuePeng
+ * date 2021/7/2 10:23
+ */
+@Getter
+@Setter
+@MappedSuperclass
+public class MetaModelCreateVo extends MetaModel {
+
+    @EruptField(
+            views = @View(title = "创建人", width = "100px"),
+            edit = @Edit(title = "创建人", readonly = @Readonly)
+    )
+    @EruptSmartSkipSerialize
+    private String createBy;
+
+    @EruptField(
+            views = @View(title = "创建时间", sortable = true),
+            edit = @Edit(title = "创建时间", readonly = @Readonly, dateType = @DateType(type = DateType.Type.DATE_TIME))
+    )
+    @EruptSmartSkipSerialize
+    private LocalDateTime createTime;
+
+}
