bca8d13cd9e0fd2ce8d37d260c6e94e94bcff6a9
yuepeng <erupts@126.com>
2022-07-08 21:53:53 +0800

    增加服务结束时销毁job能力

--- erupt-extra/erupt-job/src/main/java/xyz/erupt/job/EruptJobAutoConfiguration.java
+++ erupt-extra/erupt-job/src/main/java/xyz/erupt/job/EruptJobAutoConfiguration.java
@@ -1,5 +1,7 @@
 package xyz.erupt.job;
 
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,10 +12,12 @@ import xyz.erupt.core.module.EruptModule;
 import xyz.erupt.core.module.EruptModuleInvoke;
 import xyz.erupt.core.module.MetaMenu;
 import xyz.erupt.core.module.ModuleInfo;
+import xyz.erupt.job.config.EruptJobProp;
 import xyz.erupt.job.model.EruptJob;
 import xyz.erupt.job.model.EruptJobLog;
 import xyz.erupt.job.model.EruptMail;
-import xyz.erupt.job.service.JobStartService;
+import xyz.erupt.job.service.EruptJobService;
+import xyz.erupt.jpa.dao.EruptDao;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
@@ -29,6 +33,7 @@ import java.util.List;
 @EruptScan
 @Component
 @EnableConfigurationProperties
+@Slf4j
 public class EruptJobAutoConfiguration implements EruptModule {
 
     static {
@@ -36,11 +41,24 @@ public class EruptJobAutoConfiguration implements EruptModule {
     }
 
     @Resource
-    private JobStartService jobStartService;
+    private EruptDao eruptDao;
+
+    @Resource
+    private EruptJobService eruptJobService;
+
+    @Resource
+    private EruptJobProp eruptJobProp;
 
     @Override
+    @SneakyThrows
     public void run() {
-        jobStartService.run();
+        if (eruptJobProp.isEnable()) {
+            for (EruptJob job : eruptDao.queryEntityList(EruptJob.class, "status = true", null)) {
+                eruptJobService.modifyJob(job);
+            }
+        } else {
+            log.info("Erupt job disable");
+        }
     }
 
     @Override
