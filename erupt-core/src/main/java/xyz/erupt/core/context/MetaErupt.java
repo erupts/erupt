b2531bc80e6fb43f96db5156839b85310bd2d001
yuepeng <erupts@126.com>
2022-07-10 15:58:06 +0800

    自定义模板支持获取当前菜单能力

--- erupt-core/src/main/java/xyz/erupt/core/context/MetaErupt.java
+++ erupt-core/src/main/java/xyz/erupt/core/context/MetaErupt.java
@@ -13,8 +13,15 @@ public class MetaErupt {
 
     private String name;
 
+    private String menuValue; //当前菜单值
+
     public MetaErupt(String name) {
         this.name = name;
+    }
+
+    public MetaErupt(String name, String menuValue) {
+        this.name = name;
+        this.menuValue = menuValue;
     }
 
     public MetaErupt() {
