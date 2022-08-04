57e028c527473f2b23b95b8a2d22f05012cb4618
yuepeng <erupts@126.com>
2022-08-04 23:02:43 +0800

    ✨ erupt-jpa在初始化时根据EruptField注解标识自动添加注释

--- erupt-data/erupt-jpa/src/main/java/xyz/erupt/jpa/config/HibernateConfig.java
+++ erupt-data/erupt-jpa/src/main/java/xyz/erupt/jpa/config/HibernateConfig.java
@@ -1,8 +1,9 @@
-package xyz.erupt.jpa.support;
+package xyz.erupt.jpa.config;
 
 import org.hibernate.jpa.boot.spi.IntegratorProvider;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
 import org.springframework.stereotype.Component;
+import xyz.erupt.jpa.support.CommentIntegrator;
 
 import java.util.Collections;
 import java.util.Map;
