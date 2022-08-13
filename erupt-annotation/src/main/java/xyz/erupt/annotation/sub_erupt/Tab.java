da6af1abd8e1970382d43a99bfc2390e5f7eaf46
yuepeng <erupts@126.com>
2022-08-13 22:24:22 +0800

    upgrade to 1.10.11

--- erupt-annotation/src/main/java/xyz/erupt/annotation/sub_erupt/Tab.java
+++ erupt-annotation/src/main/java/xyz/erupt/annotation/sub_erupt/Tab.java
@@ -4,15 +4,17 @@ import xyz.erupt.annotation.config.Comment;
 
 /**
  * 多页签
+ *
  * @author YuePeng
  * date 2022/7/30 23:27
  */
+@Deprecated
 public @interface Tab {
 
-     @Comment("页签名称")
-     String name();
+    @Comment("页签名称")
+    String name();
 
-     @Comment("功能路径")
-     String path();
+    @Comment("功能路径")
+    String path();
 
 }
