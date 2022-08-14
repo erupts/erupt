83c5fb7e3dc557df42a72fe19eed133676ed7e50
yuepeng <erupts@126.com>
2022-08-15 00:07:36 +0800

    upgrade to 1.10.12 因为1.10.11版本部分文件未成功上传到maven中央仓库，所以重新上传

--- erupt-annotation/src/main/java/xyz/erupt/annotation/fun/AutoCompleteHandler.java
+++ erupt-annotation/src/main/java/xyz/erupt/annotation/fun/AutoCompleteHandler.java
@@ -11,7 +11,7 @@ import java.util.Map;
  */
 public interface AutoCompleteHandler {
 
-    List<Object> completeHandler(@Comment("前端整个表单对象") Map<String,Object> formData,
+    List<Object> completeHandler(@Comment("前端其他表单组件的值") Map<String,Object> formData,
                                  @Comment("前端输入值") String val,
                                  @Comment("注解回传参数") String[] param);
 
