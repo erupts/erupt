254c181310a98e371df663116253a3b2e755f497
yuepeng <erupts@126.com>
2022-09-16 23:05:45 +0800

    基础审计表增加默认注释

--- erupt-data/erupt-jpa/src/main/java/xyz/erupt/jpa/model/MetaModel.java
+++ erupt-data/erupt-jpa/src/main/java/xyz/erupt/jpa/model/MetaModel.java
@@ -3,6 +3,7 @@ package xyz.erupt.jpa.model;
 import lombok.Getter;
 import lombok.Setter;
 import xyz.erupt.annotation.PreDataProxy;
+import xyz.erupt.annotation.config.Comment;
 import xyz.erupt.annotation.config.EruptSmartSkipSerialize;
 
 import javax.persistence.MappedSuperclass;
@@ -18,15 +19,19 @@ import java.time.LocalDateTime;
 @PreDataProxy(MetaDataProxy.class)
 public class MetaModel extends BaseModel {
 
+    @Comment("创建人")
     @EruptSmartSkipSerialize
     private String createBy;
 
+    @Comment("创建时间")
     @EruptSmartSkipSerialize
     private LocalDateTime createTime;
 
+    @Comment("更新人")
     @EruptSmartSkipSerialize
     private String updateBy;
 
+    @Comment("更新时间")
     @EruptSmartSkipSerialize
     private LocalDateTime updateTime;
 
