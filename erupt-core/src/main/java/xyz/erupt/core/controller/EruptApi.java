36cb3e650dfea47ee33f18a7a10e4583be2fed44
李月鹏 <yuepeng.li@ideamake.cn>
2022-09-28 21:47:53 +0800

    1、erupt-core包移除erupt-machine-code接口 2、erupt-core包移除oshi依赖

--- erupt-core/src/main/java/xyz/erupt/core/controller/EruptApi.java
+++ erupt-core/src/main/java/xyz/erupt/core/controller/EruptApi.java
@@ -4,11 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import oshi.SystemInfo;
-import oshi.hardware.HardwareAbstractionLayer;
 import xyz.erupt.core.constant.EruptRestPath;
 import xyz.erupt.core.util.EruptInformation;
-import xyz.erupt.core.util.MD5Util;
 
 /**
  * @author YuePeng
@@ -22,15 +19,6 @@ public class EruptApi {
     @GetMapping("/version")
     public String version() {
         return EruptInformation.getEruptVersion();
-    }
-
-    @GetMapping(value = "/erupt-machine-code")
-    public String eruptMachineCode() {
-        //CPU序列号 + 操作系统序列号 + 硬件UUID
-        HardwareAbstractionLayer hal = new SystemInfo().getHardware();
-        return MD5Util.digest(hal.getProcessor().getProcessorIdentifier().getProcessorID()
-                + hal.getComputerSystem().getSerialNumber()
-                + hal.getComputerSystem().getHardwareUUID());
     }
 
 }
