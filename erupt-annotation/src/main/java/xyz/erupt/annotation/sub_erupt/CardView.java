8082bbe977bdf972a475d3a3e8a23a3da1ab558e
yuepeng <erupts@126.com>
2022-10-12 21:18:05 +0800

    增加画册视图的定义

--- erupt-annotation/src/main/java/xyz/erupt/annotation/sub_erupt/CardView.java
+++ erupt-annotation/src/main/java/xyz/erupt/annotation/sub_erupt/CardView.java
@@ -1,17 +1,16 @@
 package xyz.erupt.annotation.sub_erupt;
 
 /**
+ * 画册视图
  * @author YuePeng
  * date 2022/10/9 23:52
  */
 public @interface CardView {
 
-    int a = 1;
-
     boolean enable() default true;
 
     String imageField() default "";
 
-    String[] viewFields() default {};
+    String[] viewFields();
 
 }
