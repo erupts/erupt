bc7fecd6c47d5ce6484a6ca1d220f72867b61600
yuepeng <erupts@126.com>
2022-11-25 21:48:24 +0800

    修复路径冲突bug

--- erupt-upms/src/main/java/xyz/erupt/upms/base/LoginModel.java
+++ erupt-upms/src/main/java/xyz/erupt/upms/base/LoginModel.java
@@ -2,7 +2,6 @@ package xyz.erupt.upms.base;
 
 import lombok.Getter;
 import lombok.Setter;
-import xyz.erupt.upms.model.EruptMenu;
 import xyz.erupt.upms.model.EruptUser;
 
 import java.time.LocalDateTime;
@@ -15,25 +14,19 @@ import java.time.LocalDateTime;
 @Setter
 public class LoginModel {
 
-    private transient EruptUser eruptUser;
+    private boolean pass; //校验是否通过
+
+    private boolean resetPwd = false; //是否需要重置密码
 
     private boolean useVerifyCode = false; //是否需要验证码
 
-    private boolean pass; //校验是否通过
-
-    private boolean resetPwd = false; //重置密码
-
-    private String reason;  //未校验通过原因
+    private String reason; //未校验通过原因
 
     private String token;
 
-    //--------------------------
-
     private LocalDateTime expire;
 
-    private String userName;
-
-    private String indexMenu;
+    private transient EruptUser eruptUser;
 
     public LoginModel(boolean pass, EruptUser eruptUser) {
         this.pass = pass;
@@ -54,12 +47,4 @@ public class LoginModel {
     public LoginModel() {
     }
 
-    public void setEruptUser(EruptUser eruptUser) {
-        this.eruptUser = eruptUser;
-        EruptMenu indexMenu = eruptUser.getEruptMenu();
-        this.setUserName(eruptUser.getName());
-        if (null != indexMenu) {
-            this.setIndexMenu(indexMenu.getType() + "||" + indexMenu.getValue());
-        }
-    }
 }
