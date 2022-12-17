39b88680c1318330886566c51f06efcf9fcd0d69
yuepeng <erupts@126.com>
2022-12-17 21:39:40 +0800

    完善源码编译与执行使用方式

--- erupt-sample/src/main/java/xyz/erupt/sample/EruptSampleApplication.java
+++ erupt-sample/src/main/java/xyz/erupt/sample/EruptSampleApplication.java
@@ -2,10 +2,12 @@ package xyz.erupt.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import xyz.erupt.core.annotation.EruptScan;
 
 @SpringBootApplication
 @EruptScan
+@EntityScan
 public class EruptSampleApplication {
 
 	public static void main(String[] args) {
