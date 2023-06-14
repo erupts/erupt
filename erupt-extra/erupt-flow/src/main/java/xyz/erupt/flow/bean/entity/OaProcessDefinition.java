910c22bcfb93a96947ae9b7aebacd27cd048f017
zhu <hlhutu@163.com>
2023-06-14 11:15:22 +0800

    OaProcessDefinition的主键不需要生成

--- erupt-extra/erupt-flow/src/main/java/xyz/erupt/flow/bean/entity/OaProcessDefinition.java
+++ erupt-extra/erupt-flow/src/main/java/xyz/erupt/flow/bean/entity/OaProcessDefinition.java
@@ -9,7 +9,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.GenericGenerator;
 import xyz.erupt.annotation.Erupt;
 import xyz.erupt.annotation.EruptField;
 import xyz.erupt.annotation.sub_erupt.Power;
@@ -40,8 +39,6 @@ import java.util.Date;
 public class OaProcessDefinition {
 
     @Id
-    @GeneratedValue(generator = "generator")
-    @GenericGenerator(name = "generator", strategy = "native")
     @Column(name = "id")
     @EruptField(views = @View(title = "流程定义ID", sortable = true))
     @TableId(type = IdType.AUTO)
