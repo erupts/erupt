47335d65025b548aa065d9c4aea4ab99a4f866f7
yuepeng <erupts@126.com>
2023-06-22 23:14:18 +0800

    完善基于csv文件格式的i18n代码实现

--- erupt-core/src/main/java/xyz/erupt/core/prop/EruptProp.java
+++ erupt-core/src/main/java/xyz/erupt/core/prop/EruptProp.java
@@ -19,6 +19,8 @@ public class EruptProp {
     @Deprecated
     private boolean hotBuild = false;
 
+    private boolean i18n = true;
+
     //附件存储根路径
     private String uploadPath = "/opt/erupt-attachment";
 
