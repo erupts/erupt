4aa11bbcefd91f955185a99f633be709fe1010bf
yuepeng <erupts@126.com>
2023-07-08 10:44:14 +0800

    更新erupt-web编译成品

--- erupt-web/src/main/resources/public/assets/ueditor/third-party/jquery-1.10.2.js
+++ erupt-web/src/main/resources/public/assets/ueditor/third-party/jquery-1.10.2.js
@@ -4754,7 +4754,7 @@ jQuery.event = {
 			handler.guid = jQuery.guid++;
 		}
 
-		// Init the element's event structure and main handler, if this is the first
+		// Init the element's event structure and default handler, if this is the first
 		if ( !(events = elemData.events) ) {
 			events = elemData.events = {};
 		}
