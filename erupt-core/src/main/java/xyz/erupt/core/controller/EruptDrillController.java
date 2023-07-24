f496d6827aa887949c75f397a3ae4eeedd85cfe8
yuepeng <erupts@126.com>
2023-07-24 22:27:28 +0800

    重构Drill注解能力，使用请求头的方式交互，移除冗余接口

--- erupt-core/src/main/java/xyz/erupt/core/controller/EruptDrillController.java
+++ erupt-core/src/main/java/xyz/erupt/core/controller/EruptDrillController.java
@@ -1,73 +1,73 @@
-package xyz.erupt.core.controller;
-
-import com.google.gson.JsonObject;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-import xyz.erupt.annotation.Erupt;
-import xyz.erupt.annotation.sub_erupt.Link;
-import xyz.erupt.core.annotation.EruptRecordOperate;
-import xyz.erupt.core.annotation.EruptRouter;
-import xyz.erupt.core.constant.EruptRestPath;
-import xyz.erupt.core.context.MetaContext;
-import xyz.erupt.core.context.MetaErupt;
-import xyz.erupt.core.exception.EruptNoLegalPowerException;
-import xyz.erupt.core.invoke.DataProcessorManager;
-import xyz.erupt.core.naming.EruptRecordNaming;
-import xyz.erupt.core.service.EruptCoreService;
-import xyz.erupt.core.service.EruptService;
-import xyz.erupt.core.util.EruptUtil;
-import xyz.erupt.core.util.ReflectUtil;
-import xyz.erupt.core.view.EruptApiModel;
-import xyz.erupt.core.view.EruptModel;
-
-import java.lang.reflect.Field;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Stream;
-
-/**
- * @author YuePeng
- * date 2020-03-06
- */
-@RestController
-@RequestMapping(EruptRestPath.ERUPT_DATA)
-@RequiredArgsConstructor
-public class EruptDrillController {
-
-    private final EruptModifyController eruptModifyController;
-
-    private final EruptService eruptService;
-
-    @PostMapping("/add/{erupt}/drill/{code}/{id}")
-    @EruptRecordOperate(value = "新增", dynamicConfig = EruptRecordNaming.class)
-    @EruptRouter(authIndex = 2, verifyType = EruptRouter.VerifyType.ERUPT)
-    public EruptApiModel drillAdd(@PathVariable("erupt") String erupt, @PathVariable("code") String code,
-                                  @PathVariable("id") String id, @RequestBody JsonObject data) throws Exception {
-        EruptModel eruptModel = EruptCoreService.getErupt(erupt);
-        Link link = findDrillLink(eruptModel.getErupt(), code);
-        eruptService.verifyIdPermissions(eruptModel, id);
-        Map<String, Object> extraData = new HashMap<>();
-        String joinColumn = link.joinColumn();
-        Field field = ReflectUtil.findClassField(eruptModel.getClazz(), link.column());
-        field.setAccessible(true);
-        Object val = field.get(DataProcessorManager.getEruptDataProcessor(eruptModel.getClazz())
-                .findDataById(eruptModel, EruptUtil.toEruptId(eruptModel, id)));
-        if (joinColumn.contains(".")) {
-            String[] jc = joinColumn.split("\\.");
-            JsonObject jo2 = new JsonObject();
-            jo2.addProperty(jc[1], val.toString());
-            extraData.put(jc[0], jo2);
-        } else {
-            extraData.put(joinColumn, val.toString());
-        }
-        MetaContext.register(new MetaErupt(link.linkErupt().getSimpleName()));
-        return eruptModifyController.addEruptData(link.linkErupt().getSimpleName(), data, extraData);
-    }
-
-    private Link findDrillLink(Erupt erupt, String code) {
-        return Stream.of(erupt.drills()).filter(it -> code.equals(it.code()))
-                .findFirst().orElseThrow(EruptNoLegalPowerException::new).link();
-    }
-
-
-}
+//package xyz.erupt.core.controller;
+//
+//import com.google.gson.JsonObject;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.web.bind.annotation.*;
+//import xyz.erupt.annotation.Erupt;
+//import xyz.erupt.annotation.sub_erupt.Link;
+//import xyz.erupt.core.annotation.EruptRecordOperate;
+//import xyz.erupt.core.annotation.EruptRouter;
+//import xyz.erupt.core.constant.EruptRestPath;
+//import xyz.erupt.core.context.MetaContext;
+//import xyz.erupt.core.context.MetaErupt;
+//import xyz.erupt.core.exception.EruptNoLegalPowerException;
+//import xyz.erupt.core.invoke.DataProcessorManager;
+//import xyz.erupt.core.naming.EruptRecordNaming;
+//import xyz.erupt.core.service.EruptCoreService;
+//import xyz.erupt.core.service.EruptService;
+//import xyz.erupt.core.util.EruptUtil;
+//import xyz.erupt.core.util.ReflectUtil;
+//import xyz.erupt.core.view.EruptApiModel;
+//import xyz.erupt.core.view.EruptModel;
+//
+//import java.lang.reflect.Field;
+//import java.util.HashMap;
+//import java.util.Map;
+//import java.util.stream.Stream;
+//
+///**
+// * @author YuePeng
+// * date 2020-03-06
+// */
+//@RestController
+//@RequestMapping(EruptRestPath.ERUPT_DATA)
+//@RequiredArgsConstructor
+//public class EruptDrillController {
+//
+//    private final EruptModifyController eruptModifyController;
+//
+//    private final EruptService eruptService;
+//
+//    @PostMapping("/add/{erupt}/drill/{code}/{id}")
+//    @EruptRecordOperate(value = "新增", dynamicConfig = EruptRecordNaming.class)
+//    @EruptRouter(authIndex = 2, verifyType = EruptRouter.VerifyType.ERUPT)
+//    public EruptApiModel drillAdd(@PathVariable("erupt") String erupt, @PathVariable("code") String code,
+//                                  @PathVariable("id") String id, @RequestBody JsonObject data) throws Exception {
+//        EruptModel eruptModel = EruptCoreService.getErupt(erupt);
+//        Link link = findDrillLink(eruptModel.getErupt(), code);
+//        eruptService.verifyIdPermissions(eruptModel, id);
+//        Map<String, Object> extraData = new HashMap<>();
+//        String joinColumn = link.joinColumn();
+//        Field field = ReflectUtil.findClassField(eruptModel.getClazz(), link.column());
+//        field.setAccessible(true);
+//        Object val = field.get(DataProcessorManager.getEruptDataProcessor(eruptModel.getClazz())
+//                .findDataById(eruptModel, EruptUtil.toEruptId(eruptModel, id)));
+//        if (joinColumn.contains(".")) {
+//            String[] jc = joinColumn.split("\\.");
+//            JsonObject jo2 = new JsonObject();
+//            jo2.addProperty(jc[1], val.toString());
+//            extraData.put(jc[0], jo2);
+//        } else {
+//            extraData.put(joinColumn, val.toString());
+//        }
+//        MetaContext.register(new MetaErupt(link.linkErupt().getSimpleName()));
+//        return eruptModifyController.addEruptData(link.linkErupt().getSimpleName(), data, extraData);
+//    }
+//
+//    private Link findDrillLink(Erupt erupt, String code) {
+//        return Stream.of(erupt.drills()).filter(it -> code.equals(it.code()))
+//                .findFirst().orElseThrow(EruptNoLegalPowerException::new).link();
+//    }
+//
+//
+//}
