ddaca14943d78c262be25ce127b7f4cc75369a3a
yuepeng <erupts@126.com>
2023-09-01 18:42:04 +0800

    update EruptWeb

--- erupt-annotation/src/main/java/xyz/erupt/annotation/sub_field/sub_edit/CodeEditorType.java
+++ erupt-annotation/src/main/java/xyz/erupt/annotation/sub_field/sub_edit/CodeEditorType.java
@@ -3,6 +3,8 @@ package xyz.erupt.annotation.sub_field.sub_edit;
 import xyz.erupt.annotation.config.Comment;
 import xyz.erupt.annotation.fun.CodeEditHintHandler;
 
+import java.beans.Transient;
+
 /**
  * @author YuePeng
  * date 2020-02-15
@@ -15,13 +17,18 @@ public @interface CodeEditorType {
     @Comment("编辑器高度")
     int height() default 300;
 
+    @Deprecated
     @Comment("提示触发字符")
     String[] triggerCharacters() default "$";
 
+    @Deprecated
     @Comment("代码提示处理类参数")
+    @Transient
     String[] hintParams() default {};
 
+    @Deprecated
     @Comment("代码提示处理类")
+    @Transient
     Class<CodeEditHintHandler> hint() default CodeEditHintHandler.class;
 
 }
