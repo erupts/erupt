e3ca7357b39288c328d42a4fa49d17e310890382
yuepeng <erupts@126.com>
2023-10-20 00:15:31 +0800

    优化 exception日志打印过程

--- erupt-core/src/main/java/xyz/erupt/core/controller/advice/EruptExceptionAdvice.java
+++ erupt-core/src/main/java/xyz/erupt/core/controller/advice/EruptExceptionAdvice.java
@@ -1,5 +1,6 @@
 package xyz.erupt.core.controller.advice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
@@ -8,7 +9,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import xyz.erupt.core.exception.EruptApiErrorTip;
-import xyz.erupt.core.exception.EruptWebApiRuntimeException;
 import xyz.erupt.core.view.EruptApiModel;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,37 +16,41 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 
 /**
  * @author YuePeng
  * date 2020-09-30
  */
 @ControllerAdvice
+@Slf4j
 @Order(Ordered.LOWEST_PRECEDENCE - 1)
-public class EruptExceptionHandlerAdvice {
-
-    @ResponseBody
-    @ExceptionHandler(EruptWebApiRuntimeException.class)
-    public Object eruptWebApiRuntimeException(EruptWebApiRuntimeException e, HttpServletRequest request, HttpServletResponse response) {
-        Map<String, Object> map = new HashMap<>();
-        response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
-        if (e.isPrintStackTrace()) e.printStackTrace();
-        map.put("error", "erupt web api error");
-        map.put("message", e.getMessage());
-        map.put("timestamp", new Date());
-        map.put("path", request.getServletPath());
-        map.put("status", response.getStatus());
-        Optional.ofNullable(e.getExtraMap()).ifPresent(map::putAll);
-        return map;
-    }
+public class EruptExceptionAdvice {
+    public static final String API_ERROR = "erupt web api error";
+    private static final String ERE = "erupt exception";
 
     @ExceptionHandler(EruptApiErrorTip.class)
     @ResponseStatus(value = HttpStatus.OK)
     @ResponseBody
     public EruptApiModel eruptApiErrorTip(EruptApiErrorTip e) {
+        log.error(API_ERROR, e);
         e.eruptApiModel.setErrorIntercept(false);
         return e.eruptApiModel;
+    }
+
+    @ResponseBody
+    @ExceptionHandler(Exception.class)
+    public Map<String, Object> eruptRuntimeException(Exception e, HttpServletRequest request, HttpServletResponse response) {
+        response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        log.error(ERE, e);
+        Map<String, Object> map = new HashMap<>();
+        map.put("error", ERE);
+        map.put("timestamp", new Date());
+        map.put("path", request.getServletPath());
+        map.put("status", response.getStatus());
+        if (e instanceof RuntimeException) {
+            map.put("message", e.getMessage());
+        }
+        return map;
     }
 
 }
