248e88ad9daab66c5071d6afc9a225e1e9a5f8ac
yuepeng <erupts@126.com>
2023-10-21 20:25:50 +0800

    优化node节点异常处理方式

--- erupt-cloud/erupt-cloud-server/src/main/java/xyz/erupt/cloud/server/interceptor/EruptCloudServerInterceptor.java
+++ erupt-cloud/erupt-cloud-server/src/main/java/xyz/erupt/cloud/server/interceptor/EruptCloudServerInterceptor.java
@@ -9,7 +9,6 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
@@ -108,9 +107,6 @@ public class EruptCloudServerInterceptor implements WebMvcConfigurer, AsyncHandl
             if (!erupt.contains(EruptConst.DOT)) {
                 return true;
             }
-//            if (request.getServletPath().contains(EruptRestPath.ERUPT_FILE)) {
-//                return true;
-//            }
             String token = eruptContextService.getCurrentToken();
             if (null == token || null == eruptSessionService.get(SessionKey.TOKEN_OLINE + token)) {
                 response.setStatus(HttpStatus.UNAUTHORIZED.value());
@@ -135,34 +131,32 @@ public class EruptCloudServerInterceptor implements WebMvcConfigurer, AsyncHandl
             MetaContext.register(new MetaErupt(erupt));
             MetaContext.registerToken(token);
             String path = request.getRequestURI().replace(erupt, eruptName);
-            HttpResponse httpResponse = this.httpProxy(request, metaNode, path.substring(path.indexOf(EruptRestPath.ERUPT_API)), eruptName);
-            Optional.ofNullable(httpResponse.header("Content-Type")).ifPresent(response::setContentType);
-            for (String transferHeader : TRANSFER_HEADERS) {
-                Optional.ofNullable(httpResponse.header(transferHeader)).ifPresent(it -> response.addHeader(transferHeader, it));
-            }
-            response.setCharacterEncoding(StandardCharsets.UTF_8.name());
-            if (httpResponse.getStatus() == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-                if (this.isSimpleJson(httpResponse.body())) {
-                    throw new EruptWebApiRuntimeException(GsonFactory.getGson().fromJson(httpResponse.body(), Map.class), false);
-                } else {
-                    throw new EruptWebApiRuntimeException(httpResponse.body(), false);
+            try (HttpResponse httpResponse = this.httpProxy(request, metaNode, path.substring(path.indexOf(EruptRestPath.ERUPT_API)), eruptName)) {
+                Optional.ofNullable(httpResponse.header("Content-Type")).ifPresent(response::setContentType);
+                for (String transferHeader : TRANSFER_HEADERS) {
+                    Optional.ofNullable(httpResponse.header(transferHeader)).ifPresent(it -> response.addHeader(transferHeader, it));
                 }
+                response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+                if (httpResponse.getStatus() != HttpStatus.OK.value()) {
+                    response.setStatus(httpResponse.getStatus());
+                    response.sendError(httpResponse.getStatus());
+                    response.getOutputStream().write(httpResponse.bodyBytes());
+                    log.error("{}: {} -> {}", metaNode.getNodeName(), path, httpResponse.body());
+                    operationService.record(handler, new Exception(httpResponse.body()));
+                    return false;
+                } else {
+                    operationService.record(handler, null);
+                }
+                if ((EruptRestPath.ERUPT_BUILD + "/" + erupt).equals(request.getServletPath())) {
+                    EruptBuildModel eruptBuildModel = GsonFactory.getGson().fromJson(httpResponse.body(), EruptBuildModel.class);
+                    this.eruptBuildProcess(eruptBuildModel, nodeName);
+                    response.getOutputStream().write(GsonFactory.getGson().toJson(eruptBuildModel).getBytes(StandardCharsets.UTF_8));
+                } else {
+                    response.getOutputStream().write(httpResponse.bodyBytes());
+                }
+                NodeContext.remove();
+                return false;
             }
-            if (httpResponse.getStatus() != HttpStatus.OK.value()) {
-                response.sendError(httpResponse.getStatus());
-                operationService.record(handler, new Exception(httpResponse.body()));
-            } else {
-                operationService.record(handler, null);
-            }
-            if ((EruptRestPath.ERUPT_BUILD + "/" + erupt).equals(request.getServletPath())) {
-                EruptBuildModel eruptBuildModel = GsonFactory.getGson().fromJson(httpResponse.body(), EruptBuildModel.class);
-                this.eruptBuildProcess(eruptBuildModel, nodeName);
-                response.getOutputStream().write(GsonFactory.getGson().toJson(eruptBuildModel).getBytes(StandardCharsets.UTF_8));
-            } else {
-                response.getOutputStream().write(httpResponse.bodyBytes());
-            }
-            NodeContext.remove();
-            return false;
         } else {
             return true;
         }
@@ -174,16 +168,10 @@ public class EruptCloudServerInterceptor implements WebMvcConfigurer, AsyncHandl
         NodeContext.remove();
     }
 
-    private boolean isSimpleJson(String str) {
-        if (StringUtils.isBlank(str)) return false;
-        str = str.trim();
-        return str.startsWith("{") && str.endsWith("}");
-    }
-
     private int count = 0;
 
     public HttpResponse httpProxy(HttpServletRequest request, MetaNode metaNode, String path, String eruptName) {
-        if (count >= Integer.MAX_VALUE) {
+        if (count == Integer.MAX_VALUE) {
             count = 0;
         }
         String location = metaNode.getLocations().toArray(new String[0])[metaNode.getLocations().size() <= 1 ? 0 : (count++ % metaNode.getLocations().size())];
@@ -207,11 +195,7 @@ public class EruptCloudServerInterceptor implements WebMvcConfigurer, AsyncHandl
             } else {
                 httpRequest.body(StreamUtils.copyToByteArray(request.getInputStream()));
             }
-            HttpResponse httpResponse = httpRequest.addHeaders(headers).execute();
-            if (httpResponse.getStatus() != HttpStatus.OK.value()) {
-                log.warn("{} -> {}", location + path, httpResponse.body());
-            }
-            return httpResponse;
+            return httpRequest.addHeaders(headers).execute();
         } catch (Exception e) {
             throw new EruptWebApiRuntimeException(location + " -> " + e.getMessage());
         }
