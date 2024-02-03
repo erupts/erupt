87f5f31fa01b3b6edfe223328ff22859a3300109
yuepeng <erupts@126.com>
2024-02-03 23:32:51 +0800

    eruptDao新增findById方法，防止线程内多次读取导致脏读的问题

--- erupt-data/erupt-jpa/src/main/java/xyz/erupt/jpa/service/EruptDataServiceDbImpl.java
+++ erupt-data/erupt-jpa/src/main/java/xyz/erupt/jpa/service/EruptDataServiceDbImpl.java
@@ -57,13 +57,7 @@ public class EruptDataServiceDbImpl implements IEruptDataService {
 
     @Override
     public Object findDataById(EruptModel eruptModel, Object id) {
-        return entityManagerService.getEntityManager(eruptModel.getClazz(), (em) -> {
-            try {
-                return em.find(eruptModel.getClazz(), id);
-            } finally {
-                em.clear();
-            }
-        });
+        return entityManagerService.getEntityManager(eruptModel.getClazz(), (em) -> em.find(eruptModel.getClazz(), id));
     }
 
     @Override
