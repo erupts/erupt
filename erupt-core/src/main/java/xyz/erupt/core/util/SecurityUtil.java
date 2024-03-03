c5cf043e5c0fb4857f7e7e9ae3403dca37e4a4e3
yuepeng <erupts@126.com>
2024-03-03 13:30:05 +0800

    优化 xss攻击的检测过程

--- erupt-core/src/main/java/xyz/erupt/core/util/SecurityUtil.java
+++ erupt-core/src/main/java/xyz/erupt/core/util/SecurityUtil.java
@@ -23,69 +23,61 @@ public class SecurityUtil {
         if (StringUtils.isNotBlank(value)) {
             // 避免script 标签
             Pattern scriptPattern = Pattern.compile("<script>(.*?)</script>", Pattern.CASE_INSENSITIVE);
-            if (scriptPattern.matcher(value).matches()) {
+            if (scriptPattern.matcher(value).find()) {
                 return true;
             }
             // 避免src形式的表达式
             scriptPattern = Pattern.compile("src[\r\n]*=[\r\n]*\\'(.*?)\\'", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
-            if (scriptPattern.matcher(value).matches()) {
-                return true;
-            }
-            if (scriptPattern.matcher(value).matches()) {
+            if (scriptPattern.matcher(value).find()) {
                 return true;
             }
             scriptPattern = Pattern.compile("src[\r\n]*=[\r\n]*\\\"(.*?)\\\"", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
-            if (scriptPattern.matcher(value).matches()) {
-                return true;
-            }
-            // 删除单个的 </script> 标签
-            scriptPattern = Pattern.compile("</script>", Pattern.CASE_INSENSITIVE);
-            if (scriptPattern.matcher(value).matches()) {
+            if (scriptPattern.matcher(value).find()) {
                 return true;
             }
             // 删除单个的<script ...> 标签
             scriptPattern = Pattern.compile("<script(.*?)>", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
-            if (scriptPattern.matcher(value).matches()) {
+            if (scriptPattern.matcher(value).find()) {
                 return true;
             }
             // 避免 eval(...) 形式表达式
             scriptPattern = Pattern.compile("eval\\((.*?)\\)", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
-            if (scriptPattern.matcher(value).matches()) {
+            if (scriptPattern.matcher(value).find()) {
                 return true;
             }
-            // 避免 e­xpression(...) 表达式
-            scriptPattern = Pattern.compile("e­xpression\\((.*?)\\)", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
-            if (scriptPattern.matcher(value).matches()) {
+            // 避免 expression(...) 表达式
+            scriptPattern = Pattern.compile("expression\\((.*?)\\)", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+            if (scriptPattern.matcher(value).find()) {
                 return true;
             }
             // 避免 javascript: 表达式
             scriptPattern = Pattern.compile("javascript:", Pattern.CASE_INSENSITIVE);
-            if (scriptPattern.matcher(value).matches()) {
+            if (scriptPattern.matcher(value).find()) {
                 return true;
             }
-            // 避免 vbscript:表达式
+            // 避免 vbscript: 表达式
             scriptPattern = Pattern.compile("vbscript:", Pattern.CASE_INSENSITIVE);
-            if (scriptPattern.matcher(value).matches()) {
+            if (scriptPattern.matcher(value).find()) {
                 return true;
             }
             // 避免 onload= 表达式
             scriptPattern = Pattern.compile("onload(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
-            if (scriptPattern.matcher(value).matches()) {
+            if (scriptPattern.matcher(value).find()) {
                 return true;
             }
             // 避免 onmouseover= 表达式
             scriptPattern = Pattern.compile("onmouseover(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
-            if (scriptPattern.matcher(value).matches()) {
+            if (scriptPattern.matcher(value).find()) {
                 return true;
             }
             // 避免 onfocus= 表达式
             scriptPattern = Pattern.compile("onfocus(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
-            if (scriptPattern.matcher(value).matches()) {
+            if (scriptPattern.matcher(value).find()) {
                 return true;
             }
             // 避免 onerror= 表达式
-            scriptPattern = Pattern.compile("onerror(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
-            return scriptPattern.matcher(value).matches();
+            scriptPattern = Pattern.compile("^onerror(.*?)=$", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+            return scriptPattern.matcher(value).find();
         }
         return false;
     }
