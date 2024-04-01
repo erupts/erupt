5b52858b0f34d3b127360ba0f5ca707e6a264d42
yuepeng <erupts@126.com>
2024-04-01 21:43:59 +0800

    增加SQL 查询 lambda 语法支持 ：eruptDao.lambdaQuery(EruptUser.class).lik(EruptUser::getName, "e").isNul(EruptUser::getWhiteIp).addCondition("whiteIp is null").list()

---
+++ erupt-data/erupt-jpa/src/main/java/xyz/erupt/jpa/constant/SqlLang.java
@@ -0,0 +1,20 @@
+package xyz.erupt.jpa.constant;
+
+/**
+ * @author YuePeng
+ * date 2024/3/31 22:28
+ */
+public class SqlLang {
+
+    public static final String SELECT = "select ";
+
+    public static final String AS = " as ";
+
+    public static final String FROM = " from ";
+
+    public static final String WHERE = " where ";
+
+    public static final String AND = " and ";
+    public static final String ORDER_BY = " order by ";
+
+}
