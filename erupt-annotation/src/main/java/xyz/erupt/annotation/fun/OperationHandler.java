7076b03d4e45c67c4e67167a046c66d19e2d3545
yuepeng <erupts@126.com>
2024-05-25 23:25:43 +0800

    增加初始化 erupt表单值的接口定义

--- erupt-annotation/src/main/java/xyz/erupt/annotation/fun/OperationHandler.java
+++ erupt-annotation/src/main/java/xyz/erupt/annotation/fun/OperationHandler.java
@@ -8,7 +8,7 @@ import java.util.List;
  * @author YuePeng
  * date 2018-10-09.
  */
-public interface OperationHandler<@Comment("行数据类型") Target, @Comment("表单输入对象类型") EruptObject> {
+public interface OperationHandler<@Comment("行数据类型") Row, @Comment("表单输入对象类型") EruptForm> {
 
     /**
      * @param data        行数据
@@ -18,6 +18,11 @@ public interface OperationHandler<@Comment("行数据类型") Target, @Comment("
      */
     @Comment("按钮事件触发类")
     @Comment("返回值：事件触发成功后需要前端执行的 js 表达式，不需要此参数返回空即可")
-    String exec(List<Target> data, EruptObject eruptObject, String[] param);
+    String exec(List<Row> data, EruptForm eruptObject, String[] param);
+
+    @Comment("初始化 erupt 表单的值")
+    default EruptForm eruptFormValue(List<Row> data, String[] param) {
+        return null;
+    }
 
 }
