8e07f4edbd036217d2744ca3f8f8c7cbde4eee8f
yuepeng <erupts@126.com>
2024-05-26 11:28:30 +0800

    自定义按钮支持从已选数据中初始化 eruptClass 表单值

--- erupt-annotation/src/main/java/xyz/erupt/annotation/fun/OperationHandler.java
+++ erupt-annotation/src/main/java/xyz/erupt/annotation/fun/OperationHandler.java
@@ -11,18 +11,18 @@ import java.util.List;
 public interface OperationHandler<@Comment("行数据类型") Row, @Comment("表单输入对象类型") EruptForm> {
 
     /**
-     * @param data        行数据
-     * @param eruptObject 表单输入数据
-     * @param param       注解回传参数
+     * @param data      行数据
+     * @param eruptForm 表单输入数据
+     * @param param     注解回传参数
      * @return 事件触发成功后需要前端执行的 js 表达式
      */
     @Comment("按钮事件触发类")
     @Comment("返回值：事件触发成功后需要前端执行的 js 表达式，不需要此参数返回空即可")
-    String exec(List<Row> data, EruptForm eruptObject, String[] param);
+    String exec(List<Row> data, EruptForm eruptForm, String[] param);
 
     @Comment("初始化 erupt 表单的值")
-    default EruptForm eruptFormValue(List<Row> data, String[] param) {
-        return null;
+    default EruptForm eruptFormValue(List<Row> data, EruptForm eruptForm, String[] param) {
+        return eruptForm;
     }
 
 }
