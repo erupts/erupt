f18d0cce096d408456d8b0d0a9914493b71e637d
YuePeng <yuepeng.li@ideamake.cn>
2024-06-28 14:07:05 +0800

    优化日志处理逻辑

--- erupt-core/src/main/java/xyz/erupt/core/log/EruptLog4J2Appender.java
+++ erupt-core/src/main/java/xyz/erupt/core/log/EruptLog4J2Appender.java
@@ -1,8 +1,6 @@
 package xyz.erupt.core.log;
 
 import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.core.Filter;
-import org.apache.logging.log4j.core.Layout;
 import org.apache.logging.log4j.core.LogEvent;
 import org.apache.logging.log4j.core.LoggerContext;
 import org.apache.logging.log4j.core.appender.AbstractAppender;
@@ -13,6 +11,10 @@ import org.apache.logging.log4j.core.layout.PatternLayout;
 
 import java.nio.charset.StandardCharsets;
 
+/**
+ * @author YuePeng
+ * date 2024/6/27 21:58
+ */
 public class EruptLog4J2Appender implements EruptAppender {
 
     @Override
@@ -22,33 +24,33 @@ public class EruptLog4J2Appender implements EruptAppender {
         LoggerConfig logger = configuration.getRootLogger();
         PatternLayout layout = PatternLayout.newBuilder().withCharset(StandardCharsets.UTF_8)
                 .withConfiguration(configuration).withPattern("%d %t %p %X{TracingMsg} %c - %m%n").build();
-        Appender appender = new Appender(logger.getFilter(), layout);
-        appender.start();
+        AbstractAppender appender = getLog4j2Appender(logger, layout);
         configuration.addAppender(appender);
         logger.addAppender(appender, logger.getLevel(), logger.getFilter());
         context.updateLoggers(configuration);
     }
 
-    static class Appender extends AbstractAppender {
-
-        Appender(Filter filter, Layout<String> layout) {
-            super("erupt", filter, layout, false, Property.EMPTY_ARRAY);
-        }
-
-        @Override
-        public void append(LogEvent event) {
-            Formatter formatter = new Formatter()
-                    .timestamp(event.getTimeMillis())
-                    .space()
-                    .value(event.getLevel().toString())
-                    .value(" --- [ ")
-                    .value(event.getThreadName())
-                    .value("]")
-                    .value(event.getLoggerName())
-                    .value(": ")
-                    .value(event.getMessage().getFormattedMessage())
-                    .throwable(event.getThrown());
-            EruptLogManager.log(formatter.toString());
-        }
+    private static AbstractAppender getLog4j2Appender(LoggerConfig logger, PatternLayout layout) {
+        AbstractAppender appender = new AbstractAppender("erupt", logger.getFilter(), layout, false, Property.EMPTY_ARRAY) {
+            @Override
+            public void append(LogEvent event) {
+                EruptLogManager.log(new Formatter()
+                        .timestamp(event.getTimeMillis())
+                        .space()
+                        .value(event.getLevel().toString())
+                        .value(" --- [ ")
+                        .value(event.getThreadName())
+                        .value(" ] ")
+                        .value(event.getLoggerName())
+                        .value(": ")
+                        .value(event.getMessage().getFormattedMessage())
+                        .throwable(event.getThrown())
+                        .toString()
+                );
+            }
+        };
+        appender.start();
+        return appender;
     }
+
 }
