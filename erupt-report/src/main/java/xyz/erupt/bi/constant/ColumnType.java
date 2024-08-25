7c6177929327a6c99eeae59f7b20e39bc53ce1e7
yuepeng <erupts@126.com>
2024-08-25 17:00:21 +0800

    upgrade to 1.12.15

--- erupt-report/src/main/java/xyz/erupt/bi/constant/ColumnType.java
+++ erupt-report/src/main/java/xyz/erupt/bi/constant/ColumnType.java
@@ -17,25 +17,27 @@ import java.util.stream.Collectors;
 @AllArgsConstructor
 public enum ColumnType {
 
-    STRING("string", "文本"),
-    LONG_TEXT("long_text", "长文本"),
-    NUMBER("number", "数值"),
-    DATE("date", "时间"),
-    LINK("link", "链接"),
-    PERCENT("percent", "百分比"),
-    LINK_DIALOG("link_dialog", "对话框方式打开链接"),
-    DRILL("drill", "下钻"),
+    STRING("string", "文本", null),
+    NUMBER("number", "数值", "数值靠右显示"),
+    DATE("date", "时间", "时间居中显示"),
+    LONG_TEXT("long_text", "长文本", "适用于长文本场景，会自动折叠"),
+    PERCENT("percent", "百分比", "进度条方式展示百分比"),
+    LINK("link", "链接", null),
+    LINK_DIALOG("link_dialog", "对话框方式打开链接", null),
+    DRILL("drill", "下钻", null),
     ;
 
     private final String code;
 
     private final String title;
 
+    private final String desc;
+
     public static class Fetch implements ChoiceFetchHandler {
 
         @Override
         public List<VLModel> fetch(String[] params) {
-            return Arrays.stream(ColumnType.values()).map(it -> new VLModel(it.code, it.getTitle())).collect(Collectors.toList());
+            return Arrays.stream(ColumnType.values()).map(it -> new VLModel(it.code, it.getTitle(), it.getDesc())).collect(Collectors.toList());
         }
     }
 
