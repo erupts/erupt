6cd653582602f72c36b9a24e5daafe453df9ee16
yuepeng <erupts@126.com>
2024-12-27 23:16:58 +0800

    支持初始化时默认账号密码配置erupt.upms.default-account and erupt.upms.default-password

--- erupt-core/src/main/java/xyz/erupt/core/constant/EruptConst.java
+++ erupt-core/src/main/java/xyz/erupt/core/constant/EruptConst.java
@@ -8,8 +8,6 @@ public class EruptConst {
 
     public static final String ERUPT = "erupt";
 
-    public static final String ERUPT_AS = "et";
-
     public static final String BASE_PACKAGE = "xyz.erupt";
 
     public static final String ERUPT_DIR = ".erupt";
