71691b4dbf764843f2b8d3a9acd8df426b7a9711
YuePeng <erupts@126.com>
2025-01-15 12:42:12 +0800

    Fixed {{this.setXxx}} bug

--- erupt-core/src/main/java/xyz/erupt/core/view/Page.java
+++ erupt-core/src/main/java/xyz/erupt/core/view/Page.java
@@ -1,6 +1,7 @@
 package xyz.erupt.core.view;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import xyz.erupt.annotation.config.Comment;
 import xyz.erupt.annotation.model.Alert;
@@ -14,6 +15,7 @@ import java.util.Map;
  */
 @Getter
 @Setter
+@NoArgsConstructor
 public class Page {
 
     public static final int PAGE_MAX_DATA = 1000000;
@@ -28,7 +30,7 @@ public class Page {
 
     private Long total;
 
-    @Comment("Map → value 为复杂对象需做特殊处理，如：{region:{id:1,name:'xxxx'}},则需转换成：region_name 前端才可正常渲染")
+    @Comment("Map → value 复杂对象时需做特殊处理，如：{region:{id:1,name:'xxxx'}},则需转换成：region_name 前端才可正常渲染")
     private Collection<Map<String, Object>> list;
 
     private Alert alert;
@@ -45,8 +47,5 @@ public class Page {
     public Page(Integer pageIndex, Integer pageSize) {
         this.pageIndex = pageIndex;
         this.pageSize = pageSize;
-    }
-
-    public Page() {
     }
 }
