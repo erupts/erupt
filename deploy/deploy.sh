d9b083ad366c34c13a63d753aaba51494862504a
yuepeng <erupts@126.com>
2025-01-21 22:44:39 +0800

    optimize code

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,8 +5,4 @@ set -e
 # upload docker hub
 source erupt-cloud-server-docker/deploy.sh
 
-# deploy to maven center repo
-cd ../
-mvn deploy -Dmaven.test.skip=true
-
 echo "deploy scripts executed successfully!"
