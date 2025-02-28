d675e4b404d1e2423f1a40f92baf4c13181d136f
yuepeng <erupts@126.com>
2025-02-28 23:57:56 +0800

    impl ai api

--- erupt-ai/src/main/java/xyz/erupt/ai/controller/AgentController.java
+++ erupt-ai/src/main/java/xyz/erupt/ai/controller/AgentController.java
@@ -1,8 +1,51 @@
 package xyz.erupt.ai.controller;
 
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import xyz.erupt.ai.constants.ChatType;
+import xyz.erupt.ai.model.Chat;
+import xyz.erupt.ai.model.LLMAgent;
+import xyz.erupt.core.constant.EruptRestPath;
+import xyz.erupt.core.context.MetaContext;
+import xyz.erupt.core.view.R;
+import xyz.erupt.jpa.dao.EruptDao;
+
+import javax.annotation.Resource;
+import java.util.List;
+
 /**
  * @author YuePeng
  * date 2025/2/27 23:34
  */
+@Component
+@RequestMapping(EruptRestPath.ERUPT_API + "/ai/agent")
 public class AgentController {
+
+    @Resource
+    private EruptDao eruptDao;
+
+    @GetMapping("/create_agent_chat")
+    @Transactional
+    public void createAgentChat(@RequestParam Long agentId) {
+        LLMAgent llmAgent = eruptDao.lambdaQuery(LLMAgent.class).eq(LLMAgent::getId, agentId).one();
+        Chat chat = new Chat();
+        chat.setType(ChatType.AGENT);
+        chat.setTypeRef(agentId);
+        chat.setTypeName(llmAgent.getName());
+        chat.setUserId(Long.valueOf(MetaContext.getUser().getUid()));
+        eruptDao.persist(chat);
+    }
+
+    @GetMapping("/chats")
+    public R<List<Chat>> chats(@RequestParam Long agentId) {
+        return R.ok(eruptDao.lambdaQuery(Chat.class)
+                .eq(Chat::getTypeRef, agentId)
+                .eq(Chat::getUserId, Long.valueOf(MetaContext.getUser().getUid()))
+                .eq(Chat::getType, ChatType.AGENT).list());
+    }
+
+
 }
--- erupt-ai/src/main/java/xyz/erupt/ai/model/Chat.java
+++ erupt-ai/src/main/java/xyz/erupt/ai/model/Chat.java
@@ -2,12 +2,16 @@ package xyz.erupt.ai.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import xyz.erupt.ai.constants.ChatType;
 import xyz.erupt.annotation.Erupt;
 import xyz.erupt.annotation.EruptI18n;
 import xyz.erupt.jpa.model.BaseModel;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Table;
+import java.time.LocalDateTime;
 
 /**
  * @author YuePeng
@@ -21,9 +25,17 @@ import javax.persistence.Table;
 @EruptI18n
 public class Chat extends BaseModel {
 
-    private String title;
+    @Enumerated(EnumType.STRING)
+    private ChatType type;
+
+    private Long typeRef;
+
+    private String typeName;
 
     private Long userId;
 
+    private String title;
+
+    private LocalDateTime createdTime;
 
 }
