6988fc96ef0e7797d23c4554b31c8b11aa826d48
yuepeng <erupts@126.com>
2025-03-01 22:24:12 +0800

    init erupt-ai-web

---
+++ erupt-ai-web/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import {ref} from "vue";
+
+let a = ref<string>("1")
+</script>
+
+<template>
+  <div>
+    {{ a }}
+  </div>
+  <HelloWorld msg="Vite + Vue"/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ erupt-ai-web/src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import {ref} from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  {{ msg }}
+  {{ count}}
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
