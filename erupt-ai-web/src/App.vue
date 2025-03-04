b595f41f88486fddc06715df7add7ed3c4d8a224
yuepeng <erupts@126.com>
2025-03-04 22:04:53 +0800

    erupt-ai support markdown

--- erupt-ai-web/src/App.vue
+++ erupt-ai-web/src/App.vue
@@ -1,26 +1,18 @@
 <script setup lang="ts">
-import {ref} from "vue";
-import {Bubble, Sender, XStream} from "ant-design-x-vue";
+import {computed, h, ref} from "vue";
+import {Bubble, Sender} from "ant-design-x-vue";
 import {message} from "ant-design-vue";
+import 'highlight.js/styles/monokai.css';
+import md from "./markdown.ts";
 
 let content = ref<string>("")
 let windowHeight = ref<number>(window.innerHeight);
 let recording = ref<boolean>(false);
+let res = ref<string>("")
 
 window.onresize = () => {
   windowHeight.value = window.innerHeight;
 }
-
-let conversations = ref([
-  {
-    key: "item1",
-    label: "Item 1"
-  },
-  {
-    key: "item2",
-    label: "Item 2"
-  },
-])
 
 const suggestions = [
   {label: 'Write a report', value: 'report'},
@@ -41,18 +33,20 @@ const suggestions = [
   },
 ];
 
+const messageRender = (content: string) => {
+  return h('div', {innerHTML: md.render(content)});
+};
+
+
 const send = (message: string) => {
   content.value = "";
   const eventSource = new EventSource(`/erupt-api/ai/chat/send?chatId=${1}&message=${message}`); // 替换为你的 SSE 服务器地址
 
   // 监听消息事件
   eventSource.onmessage = async function (event) {
-    console.log('收到消息:', event.data);
-
-    for await (const chunk of XStream({readableStream: event.data,})) {
-      console.log(chunk);
-    }
-
+    setInterval(() => {
+      res.value = md.render(res.value + event.data)
+    }, 100)
   };
 
   // 监听错误事件
@@ -73,6 +67,12 @@ const send = (message: string) => {
     // 处理自定义事件
   });
 }
+
+const markdown = computed(() => {
+  return md.render(content.value);
+});
+
+
 </script>
 
 <template>
@@ -84,8 +84,12 @@ const send = (message: string) => {
       <div style="width: 100%;display: flex;flex-direction: column;height:100%">
         <article style="padding: 8px 0;flex: 1 1 0;overflow: auto;height: calc(100% - 66px)">
           <Bubble v-for="(item, index) in 20" :key="index" :placement="index % 2 === 0 ? 'start' : 'end'"
-                  :content="'Good morning, how are you? '+index" style="margin-bottom: 8px"
+                  :content="'###  Good morning, how are you? - 123 -23'+index" style="margin-bottom: 8px"
+                  :message-render="messageRender"
           />
+          <Bubble :placement="'start'" :content="res" :message-render="messageRender" style="margin-bottom: 8px"
+          />
+          {{ markdown }}
         </article>
         <Suggestion :items="suggestions">
           <!--              :loading='true'-->
