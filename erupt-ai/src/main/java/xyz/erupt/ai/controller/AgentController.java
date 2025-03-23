ff2aeff275c6f77953ec2a33299c7a97752661b7
yuepeng <erupts@126.com>
2025-03-23 23:32:43 +0800

    集成智能体功能

--- erupt-ai/src/main/java/xyz/erupt/ai/controller/AgentController.java
+++ erupt-ai/src/main/java/xyz/erupt/ai/controller/AgentController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import xyz.erupt.ai.model.LLMAgent;
+import xyz.erupt.ai.vo.AgentVo;
 import xyz.erupt.core.constant.EruptRestPath;
 import xyz.erupt.core.view.R;
 import xyz.erupt.jpa.dao.EruptDao;
@@ -11,6 +12,7 @@ import xyz.erupt.upms.annotation.EruptLoginAuth;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author YuePeng
@@ -25,10 +27,14 @@ public class AgentController {
 
     @EruptLoginAuth
     @GetMapping("/list")
-    public R<Void> list() {
-        List<LLMAgent> agents = eruptDao.lambdaQuery(LLMAgent.class).eq(LLMAgent::getEnable, true).list();
-
-        return R.ok();
+    public R<List<AgentVo>> list() {
+        return R.ok(eruptDao.lambdaQuery(LLMAgent.class).eq(LLMAgent::getEnable, true).list().stream().map(it->{
+            AgentVo agentVo = new AgentVo();
+            agentVo.setId(it.getId());
+            agentVo.setName(it.getName());
+            agentVo.setDesc(it.getRemark());
+            return agentVo;
+        }).collect(Collectors.toList()));
     }
 
 }
---
+++ erupt-ai/src/main/java/xyz/erupt/ai/vo/AgentVo.java
@@ -0,0 +1,20 @@
+package xyz.erupt.ai.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author YuePeng
+ * date 2025/3/23 23:30
+ */
+@Getter
+@Setter
+public class AgentVo {
+
+    private Long id;
+
+    private String name;
+
+    private String desc;
+
+}
