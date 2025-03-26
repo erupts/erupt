bafd0642f362c4a8f0f180515aa55690ba54e9fc
yuepeng <erupts@126.com>
2025-03-26 23:29:26 +0800

    集成智能体功能

--- erupt-ai/src/main/java/xyz/erupt/ai/annotation/RestAi.java
+++
@@ -1,8 +0,0 @@
-package xyz.erupt.ai.annotation;
-
-/**
- * @author YuePeng
- * date 2025/3/14 23:32
- */
-public @interface RestAi {
-}
---
+++ erupt-ai/src/main/java/xyz/erupt/ai/call/EruptCall.java
@@ -0,0 +1,22 @@
+//package xyz.erupt.ai.call;
+//
+//import org.springframework.stereotype.Component;
+//import xyz.erupt.core.context.MetaContext;
+//
+///**
+// * @author YuePeng
+// * date 2025/3/14 23:25
+// */
+//@Component
+//public class EruptCall implements AiFunction {
+//
+//    @Override
+//    public String name() {
+//        return "Erupt应用信息";
+//    }
+//
+//    @Override
+//    public String call(String param) {
+//        return "当前登录用户：" + MetaContext.getUser().getName();
+//    }
+//}
