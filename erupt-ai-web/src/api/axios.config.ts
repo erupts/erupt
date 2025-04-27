dcf3ff1432ab6c1fdd645bf5535bcd1b0b912ccd
yuepeng <erupts@126.com>
2025-04-27 22:36:25 +0800

    修复erupt-ai上下文路径访问404的问题

--- erupt-ai-web/src/api/axios.config.ts
+++ erupt-ai-web/src/api/axios.config.ts
@@ -9,7 +9,7 @@ export interface R<T> {
 }
 
 const axiosInstance = axios.create({
-    baseURL: '/',
+    // baseURL: '/',
     timeout: 5000,
     headers: {
         'Content-Type': 'application/json',
