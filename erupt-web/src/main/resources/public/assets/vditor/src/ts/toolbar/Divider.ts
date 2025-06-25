15bcca97888b185dec3133a8bc6b9e6ad70c57c6
YuePeng <erupts@126.com>
2025-06-25 23:16:52 +0800

    add markdown component

--- erupt-web/src/main/resources/public/assets/vditor/src/ts/toolbar/Divider.ts
+++
@@ -1,8 +0,0 @@
-export class Divider {
-    public element: HTMLElement;
-
-    constructor() {
-        this.element = document.createElement("div");
-        this.element.className = "vditor-toolbar__divider";
-    }
-}
--- erupt-web/src/main/resources/public/assets/vditor/src/ts/ui/setTheme.ts
+++
@@ -1,7 +0,0 @@
-export const setTheme = (vditor: IVditor) => {
-    if (vditor.options.theme === "dark") {
-        vditor.element.classList.add("vditor--dark");
-    } else {
-        vditor.element.classList.remove("vditor--dark");
-    }
-};
