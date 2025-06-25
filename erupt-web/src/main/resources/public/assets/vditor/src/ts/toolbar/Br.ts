1739c30ece5b6a49bd3166aa23a6d2859dfddd10
YuePeng <erupts@126.com>
2025-06-25 23:16:52 +0800

    add markdown component

--- erupt-web/src/main/resources/public/assets/vditor/src/ts/toolbar/Br.ts
+++
@@ -1,8 +0,0 @@
-export class Br {
-    public element: HTMLElement;
-
-    constructor() {
-        this.element = document.createElement("div");
-        this.element.className = "vditor-toolbar__br";
-    }
-}
--- erupt-web/src/main/resources/public/assets/vditor/src/ts/util/getSelectText.ts
+++
@@ -1,8 +0,0 @@
-import {selectIsEditor} from "./selection";
-
-export const getSelectText = (editor: HTMLElement, range?: Range) => {
-    if (selectIsEditor(editor, range)) {
-        return getSelection().toString();
-    }
-    return "";
-};
