5728322ee3115a4943c976ca385e5541dd5fd71d
YuePeng <erupts@126.com>
2025-07-14 22:44:07 +0800

    erupt+ai → 智能表单填充

--- erupt-ai/src/main/java/xyz/erupt/ai/controller/IntellectController.java
+++ erupt-ai/src/main/java/xyz/erupt/ai/controller/IntellectController.java
@@ -3,12 +3,11 @@ package xyz.erupt.ai.controller;
 import com.google.gson.reflect.TypeToken;
 import org.springframework.web.bind.annotation.*;
 import xyz.erupt.ai.service.LLMService;
+import xyz.erupt.ai.util.ClassTemplateUtil;
 import xyz.erupt.ai.util.MarkDownUtil;
-import xyz.erupt.annotation.sub_field.Edit;
 import xyz.erupt.core.config.GsonFactory;
 import xyz.erupt.core.constant.EruptRestPath;
 import xyz.erupt.core.service.EruptCoreService;
-import xyz.erupt.core.view.EruptFieldModel;
 import xyz.erupt.core.view.EruptModel;
 import xyz.erupt.core.view.R;
 
@@ -30,17 +29,9 @@ public class IntellectController {
     public R<Map<String, Object>> formFill(@PathVariable("erupt") String eruptName,
                                            @RequestParam(required = false) String prompt) {
         EruptModel eruptModel = EruptCoreService.getErupt(eruptName);
-        StringBuilder promptBuilder = new StringBuilder();
-        promptBuilder.append("根据如下key value 映射，随机生成json\n");
-        for (EruptFieldModel field : eruptModel.getEruptFieldModels()) {
-            Edit edit = field.getEruptField().edit();
-            if (!"".equals(edit.title())) {
-                promptBuilder.append(field.getFieldName()).append("=")
-                        .append(edit.title()).append("".equals(edit.desc()) ? "" : edit.desc()).append("\n");
-            }
-        }
-        promptBuilder.append("\n\n").append("用户的生成需求是：").append(prompt);
-        String result = llmService.send(promptBuilder.toString());
+        String promptBuilder = "根据如下面的class类结构，生成内容随机的json，注意只返回纯json\n" +
+                ClassTemplateUtil.geneEruptFormClass(eruptModel.getClazz());
+        String result = llmService.send(promptBuilder);
         return R.ok(GsonFactory.getGson().fromJson(MarkDownUtil.extractCodeBlock(result), new TypeToken<Map<String, Object>>() {
         }.getType()));
     }
