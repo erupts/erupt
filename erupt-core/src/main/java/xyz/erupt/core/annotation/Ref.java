9024574a7d5118c2b0f1472f9b3679e47a15271c
YuePeng <erupts@126.com>
2025-07-20 22:03:15 +0800

    optimize erupt-flow

--- erupt-core/src/main/java/xyz/erupt/core/annotation/Ref.java
+++ erupt-core/src/main/java/xyz/erupt/core/annotation/Ref.java
@@ -1,13 +1,15 @@
 package xyz.erupt.core.annotation;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 /**
  * @author YuePeng
  * date 2025/4/20 14:44
  */
-@Target({ElementType.FIELD})
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.TYPE})
+@Retention(java.lang.annotation.RetentionPolicy.SOURCE)
 public @interface Ref {
     Class<?> value();
 }
