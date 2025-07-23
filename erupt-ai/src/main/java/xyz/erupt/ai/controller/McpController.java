628949d0bc38719a5cb22b960283108a727c40d8
YuePeng <erupts@126.com>
2025-07-23 23:28:23 +0800

    support mcp

--- erupt-ai/src/main/java/xyz/erupt/ai/controller/McpController.java
+++ erupt-ai/src/main/java/xyz/erupt/ai/controller/McpController.java
@@ -1,7 +1,6 @@
 package xyz.erupt.ai.controller;
 
 import lombok.SneakyThrows;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.web.bind.annotation.*;
 import xyz.erupt.ai.annotation.AiParam;
 import xyz.erupt.ai.call.AiFunctionCall;
@@ -13,8 +12,10 @@ import xyz.erupt.core.util.EruptInformation;
 
 import javax.annotation.Resource;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 @RestController
 public class McpController {
@@ -22,11 +23,11 @@ public class McpController {
     @Resource
     private AiProp aiProp;
 
-    public static final String MCP_JSON = "/mcp.json";
+    private static final String MCP_JSON = "/mcp.json";
 
-    public static final String OPEN_AI_JSON = "/openapi.json";
+    private static final String OPEN_AI_JSON = "/openapi.json";
 
-    public static final String MCP_CALL_API = "/mcp/call";
+    private static final String MCP_CALL_API = "/mcp/call";
 
     @GetMapping(MCP_JSON)
     public McpVo mcp() {
@@ -52,14 +53,42 @@ public class McpController {
         for (Map.Entry<String, AiFunctionCall> entry : AiFunctionManager.getAiFunctions().entrySet()) {
             OpenAiVo.Path path = new OpenAiVo.Path();
             openAiVo.getPaths().put(MCP_CALL_API + "/" + entry.getKey(), path);
-            path.setSummary(entry.getValue().description());
-            path.setOperationId(entry.getKey());
-            OpenAiVo.RequestBody requestBody = new OpenAiVo.RequestBody();
-            path.setRequestBody(requestBody);
-            requestBody.setRequired(true);
-            OpenAiVo.ApplicationType applicationType = new OpenAiVo.ApplicationType();
-            requestBody.getContent().put("application/json", applicationType);
-            applicationType.setSchema(new OpenAiVo.Schema());
+            OpenAiVo.PathPost post = new OpenAiVo.PathPost();
+            path.setPost(post);
+            post.setSummary(entry.getValue().description());
+            post.setOperationId(entry.getKey());
+            {
+                List<String> required = new ArrayList<>();
+                Map<String, OpenAiVo.SchemaProperties> properties = new HashMap<>();
+                for (Field field : entry.getValue().getClass().getDeclaredFields()) {
+                    AiParam aiParam = field.getDeclaredAnnotation(AiParam.class);
+                    if (null != aiParam) {
+                        if (aiParam.required()) {
+                            required.add(field.getName());
+                        }
+                        OpenAiVo.SchemaProperties schemaProperties = new OpenAiVo.SchemaProperties();
+                        schemaProperties.setType(field.getType().getSimpleName());
+                        schemaProperties.setDescription(aiParam.description());
+                        properties.put(field.getName(), schemaProperties);
+                    }
+                }
+
+                OpenAiVo.RequestBody requestBody = new OpenAiVo.RequestBody();
+                post.setRequestBody(requestBody);
+                requestBody.setRequired(!properties.isEmpty());
+                OpenAiVo.ApplicationType applicationType = new OpenAiVo.ApplicationType();
+                requestBody.getContent().put("application/json", applicationType);
+                OpenAiVo.Schema schema = new OpenAiVo.Schema();
+                applicationType.setSchema(schema);
+                schema.setType("object");
+                schema.setRequired(required);
+                schema.setProperties(properties);
+            }
+            {
+                OpenAiVo.Response response = new OpenAiVo.Response();
+                post.getResponses().put(200, response);
+                response.getContent().put("text/plain", null);
+            }
         }
         return openAiVo;
     }
