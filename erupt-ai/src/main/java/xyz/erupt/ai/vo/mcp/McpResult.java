06f13020660a58c97d7529965adc3018b67985de
YuePeng <erupts@126.com>
2025-09-03 23:41:33 +0800

    erupt-ai support MCP

---
+++ erupt-ai/src/main/java/xyz/erupt/ai/vo/mcp/McpResult.java
@@ -0,0 +1,29 @@
+package xyz.erupt.ai.vo.mcp;
+
+/**
+ * @author YuePeng
+ * date 2025/9/3 23:11
+ */
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class McpResult {
+
+    private String jsonrpc = "2.0";
+
+    private Object id;
+
+    private Object result;
+
+    public McpResult(Object id, Object result) {
+        this.id = id;
+        this.result = result;
+    }
+}
