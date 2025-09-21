e2f01a00c0310ca916754b11ef8b9aee258b713b
YuePeng <erupts@126.com>
2025-09-21 22:59:44 +0800

    update erupt-web

--- erupt-web/src/main/resources/public/app.js
+++ erupt-web/src/main/resources/public/app.js
@@ -3,7 +3,7 @@ window.eruptSiteConfig = {
     domain: "",
     fileDomain: "",
     title: "Erupt Engine",
-    desc: "通用数据管理框架",
+    desc: "通用后台管理框架",
     dialogLogin: false,
     copyright: true, //是否保留显示版权信息
     // copyrightTxt: () => {
