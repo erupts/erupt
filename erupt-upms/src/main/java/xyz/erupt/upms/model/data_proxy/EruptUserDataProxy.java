b889c23c0fb1e62583248ea5927a41fbdd78d5aa
YuePeng <erupts@126.com>
2025-10-06 23:55:47 +0800

    dataProxy 增加 validate 方法用于自定义检验 erupt 数据是否符合规则

--- erupt-upms/src/main/java/xyz/erupt/upms/model/data_proxy/EruptUserDataProxy.java
+++ erupt-upms/src/main/java/xyz/erupt/upms/model/data_proxy/EruptUserDataProxy.java
@@ -3,6 +3,7 @@ package xyz.erupt.upms.model.data_proxy;
 import jakarta.annotation.Resource;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
+import xyz.erupt.annotation.exception.EruptException;
 import xyz.erupt.annotation.fun.DataProxy;
 import xyz.erupt.core.exception.EruptApiErrorTip;
 import xyz.erupt.core.exception.EruptWebApiRuntimeException;
@@ -23,8 +24,19 @@ public class EruptUserDataProxy implements DataProxy<EruptUser> {
     private EruptUserService eruptUserService;
 
     @Override
+    public void validate(EruptUser eruptUser) throws EruptException {
+        if (eruptUser.getEruptPost() != null && eruptUser.getEruptOrg() == null)
+            throw new EruptException(I18nTranslate.$translate("upms.position_no_org"));
+        EruptUser curr = eruptUserService.getCurrentEruptUser();
+        if (eruptUser.getIsAdmin()) {
+            if (null == curr.getIsAdmin() || !curr.getIsAdmin()) {
+                throw new EruptException(I18nTranslate.$translate("upms.not_super_admin-unable_add"));
+            }
+        }
+    }
+
+    @Override
     public void beforeAdd(EruptUser eruptUser) {
-        this.checkDataLegal(eruptUser);
         if (StringUtils.isBlank(eruptUser.getPasswordA())) {
             throw new EruptApiErrorTip(EruptApiModel.Status.WARNING, I18nTranslate.$translate("upms.pwd_required"), EruptApiModel.PromptWay.MESSAGE);
         }
@@ -36,22 +48,6 @@ public class EruptUserDataProxy implements DataProxy<EruptUser> {
             }
         } else {
             throw new EruptWebApiRuntimeException(I18nTranslate.$translate("upms.pwd_two_inconsistent"));
-        }
-    }
-
-    @Override
-    public void beforeUpdate(EruptUser eruptUser) {
-        this.checkDataLegal(eruptUser);
-    }
-
-    private void checkDataLegal(EruptUser eruptUser) {
-        if (eruptUser.getEruptPost() != null && eruptUser.getEruptOrg() == null)
-            throw new EruptWebApiRuntimeException(I18nTranslate.$translate("upms.position_no_org"));
-        EruptUser curr = eruptUserService.getCurrentEruptUser();
-        if (eruptUser.getIsAdmin()) {
-            if (null == curr.getIsAdmin() || !curr.getIsAdmin()) {
-                throw new EruptWebApiRuntimeException(I18nTranslate.$translate("upms.not_super_admin-unable_add"));
-            }
         }
     }
 
