225ad485572985e14bb362a7d57784bfcbfd4536
YuePeng <erupts@126.com>
2025-10-10 10:15:13 +0800

    add i18n key

--- erupt-upms/src/main/java/xyz/erupt/upms/model/online/EruptOnline.java
+++ erupt-upms/src/main/java/xyz/erupt/upms/model/online/EruptOnline.java
@@ -56,7 +56,7 @@ public class EruptOnline extends BaseModel {
     private String ip;
 
     @EruptField(
-            views = @View(title = "IP来源", desc = "格式：国家 | 大区 | 省份 | 城市 | 运营商", template = "value&&value.replace(/\\|/g,' | ')"),
+            views = @View(title = "IP来源", desc = "国家 | 大区 | 省份 | 城市 | 运营商", template = "value&&value.replace(/\\|/g,' | ')"),
             edit = @Edit(title = "IP来源", search = @Search(vague = true))
     )
     private String region;
