e9787a95199fae79326f22eff7eb93ba7b8690cc
YuePeng <erupts@126.com>
2025-10-23 18:40:00 +0800

    add unittest for ScriptUtil

--- erupt-core/src/main/java/xyz/erupt/core/util/ScriptUtil.java
+++ erupt-core/src/main/java/xyz/erupt/core/util/ScriptUtil.java
@@ -1,28 +1,35 @@
 package xyz.erupt.core.util;
 
 import lombok.SneakyThrows;
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.HostAccess;
+import org.graalvm.polyglot.PolyglotAccess;
+import org.graalvm.polyglot.Value;
 
-import javax.script.Bindings;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
 import java.util.Map;
-import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class ScriptUtil {
 
-    private static final ConcurrentLinkedQueue<ScriptEngine> POOL = new ConcurrentLinkedQueue<>();
+    private static final Context.Builder builder;
+
+    public static final String LANGUAGE_ID = "js";
+
+    static {
+        builder = Context.newBuilder(LANGUAGE_ID)
+                .option("js.ecmascript-version", "2022")
+                .allowHostAccess(HostAccess.NONE)
+                .allowPolyglotAccess(PolyglotAccess.NONE);
+    }
 
     @SneakyThrows
-    public static Object eval(String script, Map<String, Object> vars) {
-        ScriptEngine engine = POOL.poll();
-        if (engine == null) engine = new ScriptEngineManager().getEngineByName("graal.js");
-        try {
-            Bindings b = engine.createBindings();
-            b.putAll(vars);
-            return engine.eval(script, b);
-        } finally {
-            POOL.offer(engine);
+    public static Value eval(String script, Map<String, Object> vars) {
+        Context context = builder.build();
+        if (null != vars) {
+            for (Map.Entry<String, Object> entry : vars.entrySet()) {
+                context.getBindings(LANGUAGE_ID).putMember(entry.getKey(), entry.getValue());
+            }
         }
+        return context.eval(LANGUAGE_ID, script);
     }
 
 
