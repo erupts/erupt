ba216179bbb4431db18d1cd6dc6e3d898fb544df
YuePeng <erupts@126.com>
2025-11-03 09:54:46 +0800

    Erupt annotation support @Lens

--- erupt-annotation/src/main/java/xyz/erupt/annotation/lens/CalendarView.java
+++ erupt-annotation/src/main/java/xyz/erupt/annotation/lens/CalendarView.java
@@ -1,15 +1,13 @@
-package xyz.erupt.annotation.view;
+package xyz.erupt.annotation.lens;
 
 /**
  * @author YuePeng
  * date 2025/10/30 23:52
  */
-public @interface GanttView {
+public @interface CalendarView {
 
     String startDateField();
 
     String endDateField();
-
-    String pidField() default "";
 
 }
--- erupt-annotation/src/main/java/xyz/erupt/annotation/lens/GanttView.java
+++ erupt-annotation/src/main/java/xyz/erupt/annotation/lens/GanttView.java
@@ -1,13 +1,15 @@
-package xyz.erupt.annotation.view;
+package xyz.erupt.annotation.lens;
 
 /**
  * @author YuePeng
  * date 2025/10/30 23:52
  */
-public @interface CalendarView {
+public @interface GanttView {
 
     String startDateField();
 
     String endDateField();
+
+    String pidField() default "";
 
 }
