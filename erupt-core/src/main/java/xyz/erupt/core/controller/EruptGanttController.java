877325cee8c3ea421a19339b684fd8928bcad875
YuePeng <erupts@126.com>
2025-11-15 15:52:21 +0800

    add gantt update_date api

---
+++ erupt-core/src/main/java/xyz/erupt/core/controller/EruptGanttController.java
@@ -0,0 +1,70 @@
+package xyz.erupt.core.controller;
+
+import jakarta.transaction.Transactional;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import xyz.erupt.annotation.Viz;
+import xyz.erupt.annotation.fun.PowerObject;
+import xyz.erupt.core.annotation.EruptRouter;
+import xyz.erupt.core.constant.EruptRestPath;
+import xyz.erupt.core.invoke.DataProcessorManager;
+import xyz.erupt.core.service.EruptCoreService;
+import xyz.erupt.core.util.DateUtil;
+import xyz.erupt.core.util.Erupts;
+import xyz.erupt.core.util.ReflectUtil;
+import xyz.erupt.core.view.EruptModel;
+import xyz.erupt.core.view.R;
+
+import java.lang.reflect.Field;
+
+/**
+ * @author YuePeng
+ * date 2025/11/15 00:25
+ */
+@Slf4j
+@RestController
+@RequestMapping(EruptRestPath.ERUPT_DATA_MODIFY + "/gantt")
+@RequiredArgsConstructor
+public class EruptGanttController {
+
+    @PostMapping("/{erupt}/update_date")
+    @EruptRouter(skipAuthIndex = 4, authIndex = 1, verifyType = EruptRouter.VerifyType.ERUPT)
+    @Transactional
+    @SneakyThrows
+    public R<Void> updateDate(@PathVariable String erupt, @RequestBody VizGanttDateCommand command) {
+        EruptModel eruptModel = EruptCoreService.getErupt(erupt);
+        Erupts.powerLegal(eruptModel, PowerObject::isEdit);
+        for (Viz viz : eruptModel.getErupt().viz()) {
+            if (viz.code().equals(command.getVizCode())) {
+                Object obj = DataProcessorManager.getEruptDataProcessor(eruptModel.getClazz()).findDataById(eruptModel, command.getPk());
+                Field startField = ReflectUtil.findClassField(obj.getClass(), viz.ganttView().startDateField());
+                Field endField = ReflectUtil.findClassField(obj.getClass(), viz.ganttView().endDateField());
+                startField.set(obj, DateUtil.getDate(startField.getType(), command.getStartDate()));
+                endField.set(obj, DateUtil.getDate(startField.getType(), command.getEndDate()));
+                DataProcessorManager.getEruptDataProcessor(eruptModel.getClazz()).editData(eruptModel, obj);
+                break;
+            }
+        }
+        return R.ok();
+    }
+
+
+    @Getter
+    @Setter
+    public static class VizGanttDateCommand {
+
+        private String vizCode;
+
+        private String pk;
+
+        private String startDate;
+
+        private String endDate;
+
+    }
+
+}
