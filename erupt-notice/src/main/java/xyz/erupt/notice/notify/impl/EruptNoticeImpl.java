ef8e33031757edb33aa69319a2aa390fa11171b6
YuePeng <erupts@126.com>
2025-11-17 14:36:22 +0800

    add erupt-notice module

--- erupt-notice/src/main/java/xyz/erupt/notice/notify/impl/EruptNoticeImpl.java
+++ erupt-notice/src/main/java/xyz/erupt/notice/notify/impl/EruptNoticeImpl.java
@@ -2,6 +2,7 @@ package xyz.erupt.notice.notify.impl;
 
 import org.springframework.stereotype.Component;
 import xyz.erupt.notice.notify.NoticeHandler;
+import xyz.erupt.upms.model.EruptUser;
 
 @Component
 public class EruptNoticeImpl extends NoticeHandler {
@@ -11,7 +12,7 @@ public class EruptNoticeImpl extends NoticeHandler {
     }
 
     @Override
-    public void send(String title, String content) {
+    public void send(EruptUser eruptUser, String title, String content) {
 
     }
 }
