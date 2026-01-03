9f612a4016f919c684e1ccc75bc07c525061b38e
YuePeng <erupts@126.com>
2026-01-03 21:53:43 +0800

    upgrade erupt-web

--- erupt-web/src/main/resources/public/chunk-6J42UXLR.js
+++
@@ -1,9 +0,0 @@
-import {g as a} from "./chunk-ZDAHJY5Q.js";
-import "./chunk-H6LG3V7N.js";
-import "./chunk-AZU6JLO5.js";
-import "./chunk-AG4Q6ZLO.js";
-import "./chunk-F4VO4WWO.js";
-import "./chunk-LH5JAFL2.js";
-import "./chunk-WWX6BADO.js";
-
-export{a as EruptModule};
