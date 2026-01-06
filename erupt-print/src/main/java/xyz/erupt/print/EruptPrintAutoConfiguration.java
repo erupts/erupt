bd8b657cc7cf710ab0f352da2a1fc62a54c976b9
YuePeng <erupts@126.com>
2026-01-06 19:17:30 +0800

    optimize erupt-print

--- erupt-print/src/main/java/xyz/erupt/print/EruptPrintAutoConfiguration.java
+++ erupt-print/src/main/java/xyz/erupt/print/EruptPrintAutoConfiguration.java
@@ -1,11 +1,14 @@
 package xyz.erupt.print;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.Resource;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import xyz.erupt.core.module.EruptModule;
 import xyz.erupt.core.module.EruptModuleInvoke;
 import xyz.erupt.core.module.ModuleInfo;
+import xyz.erupt.upms.prop.EruptAppProp;
 
 /**
  * @author YuePeng
@@ -22,17 +25,17 @@ public class EruptPrintAutoConfiguration implements EruptModule {
         EruptModuleInvoke.addEruptModule(EruptPrintAutoConfiguration.class);
     }
 
-//    @Resource
-//    private EruptAppProp eruptAppProp;
-//
-//    @PostConstruct
-//    public void post() {
-//        eruptAppProp.registerProp(ERUPT_PRINT, true);
-//    }
+    @Resource
+    private EruptAppProp eruptAppProp;
+
+    @PostConstruct
+    public void post() {
+        eruptAppProp.registerProp(ERUPT_PRINT, true);
+    }
 
     @Override
     public ModuleInfo info() {
-        return ModuleInfo.builder().name(ERUPT_PRINT).description("Printing module").build();
+        return ModuleInfo.builder().name(ERUPT_PRINT).description("Print module").build();
     }
 
 }
--- erupt-print/src/main/java/xyz/erupt/print/model/EruptPrintLog.java
+++ erupt-print/src/main/java/xyz/erupt/print/model/EruptPrintLog.java
@@ -8,13 +8,14 @@ import xyz.erupt.annotation.Erupt;
 import xyz.erupt.annotation.EruptField;
 import xyz.erupt.annotation.sub_field.View;
 import xyz.erupt.jpa.model.MetaModelCreateOnlyVo;
+import xyz.erupt.upms.helper.HyperModelCreatorOnlyVo;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "e_print_log")
 @Erupt(name = "Print Log")
-public class EruptPrintLog extends MetaModelCreateOnlyVo {
+public class EruptPrintLog extends HyperModelCreatorOnlyVo {
 
     @EruptField(
             views = @View(title = "打印标识")
