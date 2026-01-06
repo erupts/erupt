3675cee2fa328d738902997af43c90984f69a3d3
YuePeng <erupts@126.com>
2026-01-06 21:53:46 +0800

    upgrade erupt-web

--- erupt-ai-web/src/components/markdown.ts
+++ erupt-ai-web/src/components/markdown.ts
@@ -11,34 +11,8 @@ import abbr from 'markdown-it-abbr';
 import deflist from 'markdown-it-deflist';
 import linkAttributes from 'markdown-it-link-attributes';
 import taskLists from 'markdown-it-task-lists';
-import katex from 'katex';
+import markdownItKatex from 'markdown-it-katex';
 import 'katex/dist/katex.min.css';
-
-
-// 自定义数学公式渲染
-// @ts-ignore
-const renderMath = (md) => {
-    const defaultRender = md.renderer.rules.text;
-    md.renderer.rules.text = (tokens: {
-        [x: string]: any;
-    }, idx: string | number, options: any, env: any, self: any) => {
-        const token = tokens[idx];
-        const match = token.content.match(/(\$\$?)([\s\S]*?)\1/g);
-        if (match) {
-            match.forEach((m: string) => {
-                const isBlock = m.startsWith('$$');
-                const tex = m.slice(2, -2);
-                try {
-                    const html = katex.renderToString(tex, {displayMode: isBlock});
-                    token.content = token.content.replace(m, html);
-                } catch (e: any) {
-                    token.content = token.content.replace(m, `<span class="error">Error rendering math: ${e.message}</span>`);
-                }
-            });
-        }
-        return defaultRender(tokens, idx, options, env, self);
-    };
-};
 
 // 创建 markdown-it 实例并加载插件
 const md = new MarkdownIt({
@@ -63,6 +37,6 @@ const md = new MarkdownIt({
     .use(deflist)
     .use(linkAttributes, {attrs: {target: '_blank', rel: 'noopener'}})
     .use(taskLists)
-    .use(renderMath);
+    .use(markdownItKatex);
 
 export default md;
