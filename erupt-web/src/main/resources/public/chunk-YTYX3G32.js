68646561cf0af296ab945f2cf84ffc718ae88165
YuePeng <erupts@126.com>
2026-02-03 15:07:16 +0800

    Optimize erupt cube

--- erupt-web/src/main/resources/public/chunk-YTYX3G32.js
+++ erupt-web/src/main/resources/public/chunk-YTYX3G32.js
@@ -1,10 +1,1 @@
-import {f as a} from "./chunk-AQLYC56S.js";
-import "./chunk-4FLNOXYF.js";
-import "./chunk-6GTZ5DG4.js";
-import "./chunk-YT4KRGJ2.js";
-import "./chunk-OCWN44RT.js";
-import "./chunk-EBYWMECH.js";
-import "./chunk-RXGSZPJA.js";
-import "./chunk-WWX6BADO.js";
-
-export{a as EruptModule};
+import{f as a}from"./chunk-AQLYC56S.js";import"./chunk-4FLNOXYF.js";import"./chunk-6GTZ5DG4.js";import"./chunk-YT4KRGJ2.js";import"./chunk-OCWN44RT.js";import"./chunk-EBYWMECH.js";import"./chunk-RXGSZPJA.js";import"./chunk-WWX6BADO.js";export{a as EruptModule};
