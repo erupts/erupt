4ada8a675affb18361aa010e1fb3c14b5820c924
YuePeng <erupts@126.com>
2026-03-06 23:56:08 +0800

    update erupt-web

---
+++ erupt-web/src/main/resources/public/chunk-CNFTWGLC.js
@@ -0,0 +1,10 @@
+import {h as a} from "./chunk-5CUHOYOF.js";
+import "./chunk-3YG3XQ76.js";
+import "./chunk-6GTZ5DG4.js";
+import "./chunk-OOQQJSNV.js";
+import "./chunk-GSWPJVAR.js";
+import "./chunk-OJFBL6JT.js";
+import "./chunk-S23IPLOS.js";
+import "./chunk-WWX6BADO.js";
+
+export{a as EruptModule};
