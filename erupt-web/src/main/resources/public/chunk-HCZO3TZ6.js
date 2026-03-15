7ee5be4dbe8ad9f62d82fc383a1480869955e34c
YuePeng <erupts@126.com>
2026-03-15 21:03:26 +0800

    update erupt-web

--- erupt-web/src/main/resources/public/chunk-HCZO3TZ6.js
+++ erupt-web/src/main/resources/public/chunk-HCZO3TZ6.js
@@ -1,6 +1,4 @@
-import {b as e} from "./chunk-ULEX4BWM.js";
-
-var l=e(()=>`
+import{b as e}from"./chunk-ULEX4BWM.js";var l=e(()=>`
   /* Font Awesome icon styling - consolidated */
   .label-icon {
     display: inline-block;
