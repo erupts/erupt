6b9e52046ee7e8247c8d5b90054f274b3e6e2a40
YuePeng <erupts@126.com>
2026-03-26 22:45:33 +0800

    update erupt-web

--- erupt-web/src/main/resources/public/chunk-VDNQQ5KR.js
+++
@@ -1,1 +0,0 @@
-import{h as a}from"./chunk-PSVNVHVJ.js";import"./chunk-TPAGD4GW.js";import"./chunk-2UBDDWUS.js";import"./chunk-Q564ZIL4.js";import"./chunk-AO7HMNVH.js";import"./chunk-44RKF2GX.js";import"./chunk-IXLCLQS7.js";import"./chunk-O44YI6V6.js";export{a as EruptModule};
