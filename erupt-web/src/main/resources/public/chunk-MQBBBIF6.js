9561aa64e13d277c3b2de27b80ee5b5df1e4a434
YuePeng <erupts@126.com>
2026-03-26 22:45:33 +0800

    update erupt-web

---
+++ erupt-web/src/main/resources/public/chunk-MQBBBIF6.js
@@ -0,0 +1,10 @@
+import {h as a} from "./chunk-GQNKJDXO.js";
+import "./chunk-TPAGD4GW.js";
+import "./chunk-2UBDDWUS.js";
+import "./chunk-Q564ZIL4.js";
+import "./chunk-AO7HMNVH.js";
+import "./chunk-44RKF2GX.js";
+import "./chunk-IXLCLQS7.js";
+import "./chunk-O44YI6V6.js";
+
+export{a as EruptModule};
