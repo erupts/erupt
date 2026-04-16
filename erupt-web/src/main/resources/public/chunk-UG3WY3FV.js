4f4a7104e36f653780a5dfe4a4e71b13d465873d
YuePeng <erupts@126.com>
2026-04-16 23:15:48 +0800

    update erupt-web

--- erupt-web/src/main/resources/public/chunk-UG3WY3FV.js
+++ erupt-web/src/main/resources/public/chunk-UG3WY3FV.js
@@ -1,9 +1,9 @@
-import {h as a} from "./chunk-PEU2RWDV.js";
-import "./chunk-VOC3EDAD.js";
+import {h as a} from "./chunk-NXO6LMIZ.js";
+import "./chunk-WKPHX3R7.js";
 import "./chunk-2UBDDWUS.js";
-import "./chunk-PS7WKE5X.js";
+import "./chunk-7VS7YALQ.js";
 import "./chunk-OROX4NSG.js";
-import "./chunk-W372SXQ3.js";
+import "./chunk-CE6TA4SU.js";
 import "./chunk-Z2ONH34X.js";
 import "./chunk-O44YI6V6.js";
 
