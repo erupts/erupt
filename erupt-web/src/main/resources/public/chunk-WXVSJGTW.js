4f3fb77fb1e74e01f83fcebed7a7c3321c2293a9
YuePeng <erupts@126.com>
2026-05-13 19:20:52 +0800

    update erupt-web

---
+++ erupt-web/src/main/resources/public/chunk-WXVSJGTW.js
@@ -0,0 +1,1 @@
+import{Y as s}from"./chunk-JZ5AL2PP.js";import{b as n,j as e}from"./chunk-ULEX4BWM.js";var a=n(t=>{let{securityLevel:c}=s(),o=e("body");if(c==="sandbox"){let m=e(`#i${t}`).node()?.contentDocument??document;o=e(m.body)}return o.select(`#${t}`)},"selectSvgElement");export{a};
