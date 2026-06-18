c32c9274fbd0eae4b12bc98eb586935f702da908
YuePeng <erupts@126.com>
2026-06-18 22:26:27 +0800

    Please provide the `Diff` so I can generate the appropriate commit message based on the changes you’ve made.

--- erupt-web/src/main/resources/public/chunk-CO5OR72Y.js
+++ erupt-web/src/main/resources/public/chunk-CO5OR72Y.js
@@ -1,6 +1,1 @@
-import {a} from "./chunk-NFW2BWM3.js";
-import "./chunk-MNKFFE23.js";
-import "./chunk-SDBWQRLN.js";
-import "./chunk-O44YI6V6.js";
-
-export{a as AiChatComponent};
+import{a}from"./chunk-NFW2BWM3.js";import"./chunk-MNKFFE23.js";import"./chunk-SDBWQRLN.js";import"./chunk-O44YI6V6.js";export{a as AiChatComponent};
