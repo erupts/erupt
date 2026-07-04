80b394c2de5314674f3c1e8bfdfee95e2d3558bb
YuePeng <erupts@126.com>
2026-07-04 17:55:53 +0800

    Use `GsonFactory.getGsonBuilder()` for consistent JSON formatting in `EruptOperateLog`.

--- erupt-web/src/main/resources/public/chunk-IT66AV2D.js
+++
@@ -1,8 +0,0 @@
-import {a} from "./chunk-4Q5VS4CA.js";
-import "./chunk-JSRN4HQS.js";
-import "./chunk-C26JPOMN.js";
-import "./chunk-KQHSIOME.js";
-import "./chunk-OZTEH5Z3.js";
-import "./chunk-YWJAWARJ.js";
-
-export{a as AiChatComponent};
