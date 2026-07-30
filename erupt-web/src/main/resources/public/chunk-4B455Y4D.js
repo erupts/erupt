3e86f39c7344d9095a09dc96b7164353d9917679
YuePeng <erupts@126.com>
2026-07-30 16:25:52 +0800

    upgrade erupt-web

--- erupt-web/src/main/resources/public/chunk-4B455Y4D.js
+++
@@ -1,8 +0,0 @@
-import {a} from "./chunk-P5EDNDBI.js";
-import "./chunk-Z6EOJQC3.js";
-import "./chunk-C26JPOMN.js";
-import "./chunk-ALWY6JOM.js";
-import "./chunk-OZTEH5Z3.js";
-import "./chunk-YWJAWARJ.js";
-
-export{a as AiChatComponent};
