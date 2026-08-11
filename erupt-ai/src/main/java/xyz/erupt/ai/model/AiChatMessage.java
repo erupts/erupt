4c04d9c5ca946c709bda295b25bcfca4e486385e
YuePeng <erupts@126.com>
2026-08-11 19:12:53 +0800

    AiChatMessage.java remove @Erupt

--- erupt-ai/src/main/java/xyz/erupt/ai/model/AiChatMessage.java
+++ erupt-ai/src/main/java/xyz/erupt/ai/model/AiChatMessage.java
@@ -5,30 +5,21 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import xyz.erupt.ai.constants.ChatSenderType;
-import xyz.erupt.annotation.Erupt;
-import xyz.erupt.annotation.EruptI18n;
 import xyz.erupt.annotation.constant.AnnotationConst;
-import xyz.erupt.annotation.fun.DataProxy;
-import xyz.erupt.core.util.EruptTableStyle;
 import xyz.erupt.jpa.model.BaseModel;
-import xyz.erupt.linq.lambda.LambdaSee;
 
 import java.time.LocalDateTime;
-import java.util.Collection;
-import java.util.Map;
 
 /**
  * @author YuePeng
  * date 2025/2/27 22:57
  */
-@Erupt(name = "Chat Management", dataProxy = AiChatMessage.class)
 @Getter
 @Setter
 @Table(name = "e_ai_chat_message")
 @Entity
-@EruptI18n
 @NoArgsConstructor
-public class AiChatMessage extends BaseModel implements DataProxy<AiChatMessage> {
+public class AiChatMessage extends BaseModel {
 
     private Long chatId;
 
@@ -80,13 +71,4 @@ public class AiChatMessage extends BaseModel implements DataProxy<AiChatMessage>
         return chatMessage;
     }
 
-    @Override
-    public void afterFetch(Collection<Map<String, Object>> list) {
-        String senderType = LambdaSee.field(AiChatMessage::getSenderType);
-        for (Map<String, Object> map : list) {
-            if (map.get(senderType) == ChatSenderType.MODEL) {
-                EruptTableStyle.cellColor(map, senderType, "#09f");
-            }
-        }
-    }
 }
