61d843a44e06fae1466c372e09dcb41ec9479953
YuePeng <erupts@126.com>
2026-09-03 22:13:54 +0800

    update erupt-web

--- erupt-web/src/main/resources/public/chunk-2FYMMHHK.js
+++ erupt-web/src/main/resources/public/chunk-2FYMMHHK.js
@@ -1,7 +1,7 @@
-import {b as a} from "./chunk-JU46FR3E.js";
-import "./chunk-MCXKV7N2.js";
+import {b as a} from "./chunk-QU3VN2CJ.js";
+import "./chunk-BO3BKEXD.js";
 import "./chunk-C26JPOMN.js";
-import "./chunk-GRFAS7LX.js";
+import "./chunk-H7AFU6Y3.js";
 import "./chunk-OZTEH5Z3.js";
 import "./chunk-YWJAWARJ.js";
 
