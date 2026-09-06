7bbca66866fbcc7b40d452913e17eea3e25c4f18
YuePeng <erupts@126.com>
2026-09-06 21:36:12 +0800

    update erupt-web

--- erupt-web/src/main/resources/public/app.js
+++ erupt-web/src/main/resources/public/app.js
@@ -4,8 +4,8 @@ window.eruptSiteConfig = {
     title: "Erupt Engine",
     desc: "Common Data Framework",
     dialogLogin: false,
-    copyright: true, //是否保留显示版权信息
-    tabReuse: false, //是否开启多页签，默认不开启（用户在设置面板中的选择优先于该默认值）
+    copyright: true, // whether to show the copyright footer
+    tabReuse: false, // enable multi-tab route reuse by default (the user's choice in the settings drawer takes precedence)
     logoPath: null,
     logoFoldPath: null,
     loginLogoPath: null,
@@ -13,9 +13,9 @@ window.eruptSiteConfig = {
     registerPage: null,
     amapKey: 'da01c124bff9d9be1ad44e04f23aa32e',
     amapSecurityJsCode: "5bf6c7828a97fe987c8292f00629a6d9",
-    theme: {
-        primaryColor: '#3f51b5'
-    },
+    // theme: {
+    //     primaryColor: 'rgb(22, 119, 255)'
+    // },
     r_tools: [{
         mobileHidden: true,
         render: () => {
--- erupt-web/src/main/resources/public/chunk-GMXO6N2Y.js
+++ erupt-web/src/main/resources/public/chunk-GMXO6N2Y.js
@@ -1,5 +1,5 @@
-import {b as a} from "./chunk-OMS2U3L4.js";
-import "./chunk-LDCXD236.js";
+import {b as a} from "./chunk-ZYIVRI6L.js";
+import "./chunk-32XHIPL5.js";
 import "./chunk-C26JPOMN.js";
 import "./chunk-H7AFU6Y3.js";
 import "./chunk-OZTEH5Z3.js";
