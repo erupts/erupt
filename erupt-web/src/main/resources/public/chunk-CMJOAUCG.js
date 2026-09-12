c2ac720329507b1da89d6e7e826de5b8d2bbf8f0
YuePeng <erupts@126.com>
2026-09-13 00:23:24 +0800

    update erupt-web

---
+++ erupt-web/src/main/resources/public/chunk-CMJOAUCG.js
@@ -0,0 +1,10 @@
+import {b as a} from "./chunk-4HVFM3OP.js";
+import "./chunk-A2UFIJFL.js";
+import "./chunk-MRL64V4X.js";
+import "./chunk-C26JPOMN.js";
+import "./chunk-ZEEK2OIF.js";
+import "./chunk-VZRRCFTM.js";
+import "./chunk-OZTEH5Z3.js";
+import "./chunk-YWJAWARJ.js";
+
+export{a as AiChatComponent};
