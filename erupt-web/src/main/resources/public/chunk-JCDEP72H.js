68fa4df904f6be5193053816b9d9d8410c105db2
YuePeng <erupts@126.com>
2026-09-22 22:34:16 +0800

    update erupt-web

--- erupt-web/src/main/resources/public/chunk-JCDEP72H.js
+++ erupt-web/src/main/resources/public/chunk-JCDEP72H.js
@@ -1,10 +1,1 @@
-import {b as a} from "./chunk-FZJTSC3F.js";
-import "./chunk-VPECJS4B.js";
-import "./chunk-FXZ3WJMW.js";
-import "./chunk-6ZOXJ5QE.js";
-import "./chunk-3DOTEWEI.js";
-import "./chunk-XRI3JTKN.js";
-import "./chunk-6LO4QN6V.js";
-import "./chunk-YWJAWARJ.js";
-
-export{a as AiChatComponent};
+import{b as a}from"./chunk-FZJTSC3F.js";import"./chunk-VPECJS4B.js";import"./chunk-FXZ3WJMW.js";import"./chunk-6ZOXJ5QE.js";import"./chunk-3DOTEWEI.js";import"./chunk-XRI3JTKN.js";import"./chunk-6LO4QN6V.js";import"./chunk-YWJAWARJ.js";export{a as AiChatComponent};
